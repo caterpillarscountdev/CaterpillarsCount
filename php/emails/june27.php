3f35717a4f636c1fe761ea6284e8a15835648d9d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 03:37:33 -0500

    remove mysqli_num_rows($query) == 0 ||

--- php/emails/june27.php
+++ php/emails/june27.php
@@ -11,7 +11,7 @@
 	for($i = 0; $i < count($sites); $i++){
 		if($sites[$i]->getActive() && $sites[$i]->getNumberOfSurveysByYear(date("Y")) <= 2){
 			$query = mysqli_query($dbconn, "SELECT COUNT(Survey.ID) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $sites[$i]->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
-			if(mysqli_num_rows($query) == 0 || intval(mysqli_fetch_assoc($query)["Count"]) == 0){
+			if(intval(mysqli_fetch_assoc($query)["Count"]) == 0){
 				$emails = $sites[$i]->getAuthorityEmails();
 				for($j = 0; $j < count($emails); $j++){
 					email6($emails[$j], "Touching Base about " . $sites[$i]->getName(), $sites[$i]->getName());
