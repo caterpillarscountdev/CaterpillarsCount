698a2aabcfda1af5903fbbc531b79176d28c97f2
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-15 22:01:30 -0500

    Delete sendToDevOutlook.php

--- php/emails/sendToDevOutlook.php
+++
@@ -1,8 +0,0 @@
-<?php
-  header('Access-Control-Allow-Origin: *');
-  
-  require_once("../orm/Site.php");
-  require_once("../orm/resources/mailing.php");
-
-  email8("paul.plocharczyk@ihop.com", "Your Caterpillars Count! weekly summary", array(Site::findByID("2")), 57, 3, "hurlbert", true);
-?>
