9b3638af45eaaa33f75d2b2a643183d529f37031
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 01:34:26 -0500

    add missing query

--- php/emails/testStats.php
+++ php/emails/testStats.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Origin: *');
 	require_once("../orm/resources/mailing.php");
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT Survey.LocalDate) AS DateCount, COUNT(DISTINCT Survey.UserFKOfObserver) AS ParticipantCount, COUNT(DISTINCT Survey.PlantFK) AS PlantCount, COUNT(DISTINCT Plant.Circle) AS SurveyedCircleCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE YEAR(Survey.LocalDate)='" . (intval(date("Y")) - 1) . "' AND Plant.SiteFK='2' GROUP BY Plant.SiteFK");
   $site = Site::findByID("2");
 		if($site->getLatitude() < 36.5){
 			$siteName = $site->getName();
