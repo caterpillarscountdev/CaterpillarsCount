cd50704fb88ce0abf09d95eb77a7462e5a53db36
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 03:04:43 -0500

    remove extra $

--- php/emails/june27.php
+++ php/emails/june27.php
@@ -10,7 +10,7 @@
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	for($i = 0; $i < count($sites); $i++){
 		if($sites[$i]->getActive() && $sites[$i]->getNumberOfSurveysByYear(date("Y")) <= 2){
-			$query = mysqli_query($dbconn, "SELECT COUNT(Survey.*) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
+			$query = mysqli_query($dbconn, "SELECT COUNT(Survey.*) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $sites[$i]->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
 			if(intval(mysqli_fetch_assoc($query)["Count"]) == 0){
 				$emails = $sites[$i]->getAuthorityEmails();
 				for($j = 0; $j < count($emails); $j++){
