e0d6294c4ae3f8fed97412efd665c93eb579f74b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-15 20:59:17 -0500

    Update sendToDevOutlook.php

--- php/emails/sendToDevOutlook.php
+++ php/emails/sendToDevOutlook.php
@@ -1,1 +1,8 @@
-email8("sarah.yelton@unc.edu", "Your Caterpillars Count! weekly summary", array(Site::findByID("2")), 57, 3, "hurlbert", true);
+<?php
+  header('Access-Control-Allow-Origin: *');
+  
+  require_once("../orm/Site.php");
+  require_once("../orm/resources/mailing.php");
+
+  email8("paul.plocharczyk@ihop.com", "Your Caterpillars Count! weekly summary", array(Site::findByID("2")), 57, 3, "hurlbert", true);
+?>
