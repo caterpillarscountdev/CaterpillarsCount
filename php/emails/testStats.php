896b986b0b95258a6726c3ab77fb3d12f2d0aa0a
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 02:39:42 -0500

    Update testStats.php

--- php/emails/testStats.php
+++ php/emails/testStats.php
@@ -5,21 +5,28 @@ header('Access-Control-Allow-Origin: *');
 	require_once("../orm/resources/Keychain.php");
 	require_once("../orm/resources/mailing.php");
 	
-	$dbconn = (new Keychain)->getDatabaseConnection();
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT Survey.LocalDate) AS DateCount, COUNT(DISTINCT Survey.UserFKOfObserver) AS ParticipantCount, COUNT(DISTINCT Survey.PlantFK) AS PlantCount, COUNT(DISTINCT Plant.Circle) AS SurveyedCircleCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE YEAR(Survey.LocalDate)='" . (intval(date("Y")) - 1) . "' AND Plant.SiteFK='2' GROUP BY Plant.SiteFK");
-	$siteRow = mysqli_fetch_assoc($query);
 	$site = Site::findByID("2");
-	if($site->getActive() && $site->getLatitude() < 36.5 && $site->getNumberOfSurveysByYear("2019") == 0){
-      $emails = $site->getAuthorityEmails();
+	$emails = $sites[$i]->getAuthorityEmails();
+      
+      $dbconn = (new Keychain)->getDatabaseConnection();
+      $query = mysqli_query($dbconn, "SELECT COUNT(*) AS `All`, SUM(SubmittedThroughApp) AS `App` FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $site->getID() . "' AND YEAR(LocalDate)='" . (intval(date("Y")) - 1) . "'");
+      mysqli_close($dbconn);
+      $resultRow = mysqli_fetch_assoc($query);
+      $all = intval($resultRow["All"]);
+      $app = intval($resultRow["App"]);
+      
       for($j = 0; $j < count($emails); $j++){
         $firstName = "there";
         $user = User::findByEmail($emails[$j]);
         if(is_object($user) && get_class($user) == "User"){
           $firstName = $user->getFirstName();
         }
-	      if($emails[$j] == "plocharczykweb@gmail.com"){
-        email4($emails[$j], "The Caterpillars Count! Season Has Begun!", $firstName);
-	      }
-      }
-    }
+        if($emails[$j] == "plocharczykweb@gmail.com"){
+		if($all == 0 || $app > ($all / 2)){
+		  email4($emails[$j], "The Caterpillars Count! Season Has Begun!", $firstName);
+		}
+		else{
+		  email5($emails[$j], "Need Help Submitting Caterpillars Count! Surveys?", $firstName);
+		}
+	}
 ?>
