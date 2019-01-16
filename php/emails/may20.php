81b628af9c3290a512cc76556272359df94f074a
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 00:01:19 -0500

    give param to findByEmail

--- php/emails/may20.php
+++ php/emails/may20.php
@@ -11,7 +11,7 @@
       $emails = $sites[$i]->getAuthorityEmails();
       for($j = 0; $j < count($emails); $j++){
         $firstName = "there";
-        $user = User::findByEmail();
+        $user = User::findByEmail($email[$j]);
         if(is_object($user) && get_class($user) != "User"){
           $firstName = $user->getFirstName();
         }
