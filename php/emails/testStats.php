856036b85f6baaec9b158fb0146c0209b685a6d9
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-01-16 03:23:29 -0500

    try 2018

--- php/emails/testStats.php
+++ php/emails/testStats.php
@@ -7,7 +7,7 @@ header('Access-Control-Allow-Origin: *');
 	
 	$site = Site::findByID("2");
 	$dbconn = (new Keychain)->getDatabaseConnection();
-      $query = mysqli_query($dbconn, "SELECT COUNT(Survey.ID) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $site->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
+      $query = mysqli_query($dbconn, "SELECT COUNT(Survey.ID) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $site->getID() . "' AND Survey.LocalDate>'2018-06-13'");
 			if(intval(mysqli_fetch_assoc($query)["Count"]) == 0){
 				$emails = $site->getAuthorityEmails();
 				for($j = 0; $j < count($emails); $j++){
