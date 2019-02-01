99df9385db67e216062a5f7f1c2b09a7b33f2b9a
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-02-01 12:41:30 -0500

    Create submitAnalysisToSciStarter.php

---
+++ php/submitAnalysisToSciStarter.php
@@ -0,0 +1,11 @@
+<?php
+  require_once("orm/User.php");
+  require_once("submitToSciStarter.php");
+  
+	$email = trim(rawurldecode($_POST["email"]));
+  
+  $user = User::findByEmail($email);
+  if(is_object($user) || get_class($user) == "User"){
+    submitToSciStarter($user->getEmail(), "classification", null, date("Y-m-d") . "T" . date("H:i:s"), null, 1, null);
+  }
+?>
