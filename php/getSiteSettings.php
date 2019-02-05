1eee253659c6fb778eabb720595d0deb45d9915f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-02-05 13:46:57 -0500

    compare IDs instead of objects

--- php/getSiteSettings.php
+++ php/getSiteSettings.php
@@ -4,15 +4,18 @@
 	require_once('orm/User.php');
 	require_once('orm/Site.php');
 
-	$siteID = $_GET["siteID"];
+	$siteID = intval($_GET["siteID"]);
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
 
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$sites = $user->getSites();
+		for($i = 0; $i < count($sites); $i++){
+			$sites[$i] = intval($sites[$i]->getID());
+		}
 		$site = Site::findByID($siteID);
-		if(is_object($site) && get_class($site) == "Site" && in_array($site, $sites)){
+		if(is_object($site) && get_class($site) == "Site" && in_array($siteID, $sites)){
 			$siteArray = array(
 				"name" => $site->getName(),
 				"description" => $site->getDescription(),
