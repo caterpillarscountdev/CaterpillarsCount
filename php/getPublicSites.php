36a5c8be2c82b8c9f2bd8532e26cfd0c6566ee9e
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-02-15 22:21:35 -0500

    remove reference to outdated site

--- php/getPublicSites.php
+++ php/getPublicSites.php
@@ -20,22 +20,6 @@
 			"longitude" => $publicSites[$i]->getLongitude(),
 		);
 	}
-	
-	/*
-	//GRANDFATHER IN OLD, INACTIVE SITE:
-	$staticSites = array(
-		array(
-			"creatorEmailParts" => array("max.cawley", "lifeandscience.org"),
-		    	"siteName" => "North Carolina Museum of Life and Science",
-		    	"description" => "Our interactive science park includes a two-story science center, one of the largest butterfly conservatories on the East Coast and beautifully landscaped outdoor exhibits",
-		    	"region" => "NC",
-		    	"latitude" => 36.0294,
-		    	"longitude" => -78.9016,
-		),
-	);
-
-	$publicSitesArray = array_merge($publicSitesArray, $staticSites);
-	*/
 
 	die(json_encode($publicSitesArray));
 ?>
