a94c6be548e5e351973c8ebd5517c9035c21e9c3
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-02-27 02:00:02 -0500

    die $lastCall as well

--- php/getSiteAnalysis.php
+++ php/getSiteAnalysis.php
@@ -4,8 +4,8 @@
 	require_once('orm/resources/Keychain.php');
 	require_once('orm/Site.php');
 
-	$start = $_GET["start"];
-	$LIMIT = $_GET["limit"];
+	$start = intval($_GET["start"]);
+	$LIMIT = intval($_GET["limit"]);
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	
@@ -20,6 +20,7 @@
 	$firstSurveyYear = $row["FirstSurveyYear"];
 	$lastSurveyYear = $row["LastSurveyYear"];
 	$siteIDs = array();
+	$lastCall = ($start + $LIMIT) >= intval(mysqli_fetch_assoc(mysqli_query($dbconn, "SELECT COUNT(*) AS Count FROM Site"))["Count"]);
 	for($i = 0; $i < count($sites); $i++){
 		if($sites[$i]->getID() != 2){
 			$surveysEachWeek = array();
@@ -27,8 +28,10 @@
 				$surveysEachWeek[] = 0;
 			}
 			$query = mysqli_query($dbconn, "SELECT WEEK(Survey.LocalDate, 1) AS Week, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='" . $sites[$i]->getID() . "' AND YEAR(Survey.LocalDate)='$lastSurveyYear' GROUP BY WEEK(Survey.LocalDate, 1)");
-			while($row = mysqli_fetch_assoc($query)){
-				$surveysEachWeek[intval($row["Week"]) - $startWeek] = intval($row["SurveyCount"]);
+			if(mysqli_num_rows($query) > 0){
+				while($row = mysqli_fetch_assoc($query)){
+					$surveysEachWeek[intval($row["Week"]) - $startWeek] = intval($row["SurveyCount"]);
+				}
 			}
 			
 			$siteIDs[] = $sites[$i]->getID();
@@ -37,8 +40,15 @@
 				"name" => $sites[$i]->getName(),
 				"url" => $sites[$i]->getURL(),
 				"authorities" => array($sites[$i]->getCreator()->getFullName(), $sites[$i]->getCreator()->getEmail()),
-				"surveysEachWeek" => $surveysEachWeek
+				"surveysEachWeek" => $surveysEachWeek,
+				"firstSurveyYear" => "N/A",
+				"plantCount" => 0,
+				"observerCount" => 0,
+				"mostRecentSurveyDate" => "N/A"
 			);
+		}
+		else if(count($sites) == 1){
+			die("true|" . json_encode(array($firstSurveyYear, $lastSurveyYear, $data)));
 		}
 	}
 	
@@ -92,5 +102,5 @@
 		}
 	}
 
-	die("true|" . json_encode(array($firstSurveyYear, $lastSurveyYear, $data)));
+	die("true|" . json_encode(array($firstSurveyYear, $lastSurveyYear, $data, $lastCall)));
 ?>
