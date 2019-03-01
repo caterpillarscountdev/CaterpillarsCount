d70b4c9b805147b5ab05c339eb25dbb08c0b733f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-03-01 01:50:32 -0500

    return surveys by week for every site

--- php/getWeeklySurveysByYear.php
+++ php/getWeeklySurveysByYear.php
@@ -3,32 +3,38 @@
   	
 	require_once('orm/resources/Keychain.php');
 	require_once('orm/Site.php');
-  
-	$siteID = intval($_GET["siteID"]);
-	$year = intval($_GET["year"]);
-  
-	$site = Site::findByID($siteID);
-	if(is_object($site) && get_class($site) == "Site"){
-		$dbconn = (new Keychain)->getDatabaseConnection();
-		
-		//surveys each week, separated by year
-		$query = mysqli_query($dbconn, "SELECT WEEK(\"$year-01-01\") AS StartWeek, WEEK(\"$year-12-31\") AS EndWeek");
-		$row = mysqli_fetch_assoc($query);
-		$startWeek = intval($row["StartWeek"]);
-		$endWeek = intval($row["EndWeek"]);
-		
-		$surveysEachWeek = array();
-		for($i = $startWeek; $i <= $endWeek; $i++){
-			$surveysEachWeek[] = 0;
+	$start = intval($_GET["start"]);
+	$LIMIT = 50;
+	
+	$dbconn = (new Keychain)->getDatabaseConnection();
+	
+	$data = array();
+	$sites = Site::findAll($start, $LIMIT);
+	$query = mysqli_query($dbconn, "SELECT WEEK(\"" . $lastSurveyYear . "-01-01\") AS StartWeek, WEEK(\"" . $lastSurveyYear . "-12-31\") AS EndWeek");
+	$row = mysqli_fetch_assoc($query);
+	$startWeek = intval($row["StartWeek"]);
+	$endWeek = intval($row["EndWeek"]);
+	$siteIDs = array();
+	$lastCall = ($start + $LIMIT) >= intval(mysqli_fetch_assoc(mysqli_query($dbconn, "SELECT COUNT(*) AS Count FROM Site"))["Count"]);
+	for($i = 0; $i < count($sites); $i++){
+		if($sites[$i]->getID() != 2){
+			$surveysEachWeek = array();
+			for($j = $startWeek; $j <= $endWeek; $j++){
+				$surveysEachWeek[] = 0;
+			}
+			$query = mysqli_query($dbconn, "SELECT WEEK(Survey.LocalDate, 1) AS Week, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='" . $sites[$i]->getID() . "' AND YEAR(Survey.LocalDate)='$lastSurveyYear' GROUP BY WEEK(Survey.LocalDate, 1)");
+			if(mysqli_num_rows($query) > 0){
+				while($row = mysqli_fetch_assoc($query)){
+					$surveysEachWeek[intval($row["Week"]) - $startWeek] = intval($row["SurveyCount"]);
+				}
+			}
+			
+			$siteIDs[] = $sites[$i]->getID();
+			$data[(string)$sites[$i]->getID()] = $surveysEachWeek;
 		}
-		$query = mysqli_query($dbconn, "SELECT WEEK(Survey.LocalDate, 1) AS Week, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' AND YEAR(Survey.LocalDate)='$year' GROUP BY WEEK(Survey.LocalDate, 1)");
-		while($row = mysqli_fetch_assoc($query)){
-			$surveysEachWeek[intval($row["Week"]) - $startWeek] = intval($row["SurveyCount"]);
+		else if(count($sites) == 1){
+			die("true|" . json_encode(array($data, true));
 		}
-		
-		mysqli_close($dbconn);
-    
-		die("true|" . json_encode($surveysEachWeek));
 	}
-	die("false|We could not find this site.");
+	die("true|" . json_encode(array($data, $lastCall)));
 ?>
