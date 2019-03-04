5d95adb8ebcdf9de42f544111f47fe226630e947
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-03-04 16:55:48 -0500

    change error message

--- php/getUserBackups.php
+++ php/getUserBackups.php
@@ -16,5 +16,5 @@
     		}
     		die("true|" . json_encode(array_values($files)));
   	}
-  	die("false|Please log in with a developer account to view this page.");
+  	die("false|Please log in with a developer account before accessing this page.");
 ?>
