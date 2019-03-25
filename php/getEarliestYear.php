432a238545d2bc06686ccf2f0156c1261e32ef4d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-03-25 15:39:54 -0400

    add memory

--- php/getEarliestYear.php
+++ php/getEarliestYear.php
@@ -2,6 +2,18 @@
 	require_once('orm/resources/Keychain.php');
 
 	$siteID = $_GET["siteID"];
+
+	$HIGH_TRAFFIC_MODE = true;
+	$SAVE_TIME_LIMIT = 20;
+	
+	$baseFileName = basename(__FILE__, '.php') . $siteID;
+	if($HIGH_TRAFFIC_MODE){
+		$save = getSave($baseFileName, $SAVE_TIME_LIMIT);
+		if($save !== null){
+			die($save);
+		}
+	}
+	
 	$extraSQL = "";
 	if(isset($siteID)){
 		$extraSQL = " JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='" . intval($siteID) . "'";
@@ -10,5 +22,10 @@
 	$dbconn = (new Keychain)->getDatabaseConnection();
   
 	$query = mysqli_query($dbconn, "SELECT YEAR(LocalDate) AS EarliestYear FROM Survey" . $extraSQL . " ORDER BY LocalDate ASC LIMIT 1");
-	die(mysqli_fetch_assoc($query)["EarliestYear"]);
+	
+	$result = "" . mysqli_fetch_assoc($query)["EarliestYear"];
+	if($HIGH_TRAFFIC_MODE){
+		save($baseFileName, $result);
+	}
+	die($result);
 ?>
