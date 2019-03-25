7bd857eae62390af6363cf09136fa76dbf7620c7
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-03-25 14:57:35 -0400

    fix indentation

--- php/resultMemory.php
+++ php/resultMemory.php
@@ -1,21 +1,21 @@
 <?php
-  function save($baseFileName, $result){
-    $saveFilePath = $baseFileName . ".txt";
+	function save($baseFileName, $result){
+		$saveFilePath = $baseFileName . ".txt";
 		$saveFile = fopen($saveFilePath, "w");
 		fwrite($saveFile, time() . "|" . $result);
 		fclose($saveFile);
-  }
+	}
   
-  function getSave($baseFileName, $timeLimit){
-    $saveFilePath = $baseFileName . ".txt";
-    if(file_exists($saveFilePath)){
-      $contents = file_get_contents($saveFilePath);
-      if($contents !== false){
-        $savedTime = intval(substr($contents, 0, strpos($contents, "|")));
-        if(time() - $savedTime <= $timeLimit){
-          die(substr($contents, (strpos($contents, "|") + 1)));
-        }
-      }
-    }
-  }
+	function getSave($baseFileName, $timeLimit){
+		$saveFilePath = $baseFileName . ".txt";
+		if(file_exists($saveFilePath)){
+			$contents = file_get_contents($saveFilePath);
+			if($contents !== false){
+				$savedTime = intval(substr($contents, 0, strpos($contents, "|")));
+				if(time() - $savedTime <= $timeLimit){
+					return substr($contents, (strpos($contents, "|") + 1));
+				}
+			}
+		}
+	}
 ?>
