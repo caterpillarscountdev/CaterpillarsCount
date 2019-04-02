4de896918422d32c84ec109aee3ddcd339c671dc
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-04-02 03:39:32 -0400

    add min survey requirement for global leaderboard

--- php/getUserRankings.php
+++ php/getUserRankings.php
@@ -8,6 +8,8 @@
 	$HIGH_TRAFFIC_MODE = true;
 	$SAVE_TIME_LIMIT = 60;
 	
+	$MIN_SURVEY_REQUIREMENT = 10;//only for the global leaderboard- doesn't apply to the site-specific leaderboard
+	
 	$baseFileName = basename(__FILE__, '.php') . $siteID;
 	if($HIGH_TRAFFIC_MODE){
 		$save = getSave($baseFileName, $SAVE_TIME_LIMIT);
@@ -17,10 +19,14 @@
 	}
 
 	$siteRestriction = "<>2";
-	if($siteID > 0){$siteRestriction = "=" . $siteID;}
+	$minSurveyRestriction = " AND Total>='$MIN_SURVEY_REQUIREMENT'";
+	if($siteID > 0){
+		$siteRestriction = "=" . $siteID;
+		$minSurveyRestriction = "";
+	}
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
-	$query = mysqli_query($dbconn, "SELECT User.ID, CONCAT(User.FirstName, ' ', User.LastName) AS FullName, User.Hidden, SUM(CASE WHEN Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) THEN 1 ELSE 0 END) AS Week, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Month, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Year, Count(*) AS Total, COUNT(DISTINCT Survey.LocalDate) AS TotalUniqueDates FROM `Survey` JOIN User ON Survey.UserFKOfObserver=User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " GROUP BY User.ID ORDER BY Year DESC");
+	$query = mysqli_query($dbconn, "SELECT User.ID, CONCAT(User.FirstName, ' ', User.LastName) AS FullName, User.Hidden, SUM(CASE WHEN Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) THEN 1 ELSE 0 END) AS Week, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Month, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Year, Count(*) AS Total, COUNT(DISTINCT Survey.LocalDate) AS TotalUniqueDates FROM `Survey` JOIN User ON Survey.UserFKOfObserver=User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . $minSurveyRestriction . " GROUP BY User.ID ORDER BY Year DESC");
 	
 	$rankingsArray = array();
   	$i = 1;
@@ -44,28 +50,28 @@
     		);
 	}
 	
-	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisWeek FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) GROUP BY UserFKOfObserver");
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisWeek FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE UserFKOfObserver IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisWeek"] = intval($row["UniqueDatesThisWeek"]);
 	}
 	
-	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisMonth FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisMonth FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE UserFKOfObserver IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisMonth"] = intval($row["UniqueDatesThisMonth"]);
 	}
 
-	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE UserFKOfObserver IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisYear"] = intval($row["UniqueDatesThisYear"]);
 	}
 
-	$query = mysqli_query($dbconn, "SELECT Survey.UserFKOfObserver, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS Caterpillars FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND ArthropodSighting.Group='caterpillar' GROUP BY Survey.UserFKOfObserver");
+	$query = mysqli_query($dbconn, "SELECT Survey.UserFKOfObserver, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS Caterpillars FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE UserFKOfObserver IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK" . $siteRestriction . " AND ArthropodSighting.Group='caterpillar' GROUP BY Survey.UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["UserFKOfObserver"])]["Caterpillars"] = round(((floatval($row["Caterpillars"]) / floatval($rankingsArray[strval($row["UserFKOfObserver"])]["Total"])) * 100), 2) . "%";
 	}
 	mysqli_close($dbconn);
 	
-	if($siteID <= 0){
+	if($siteID <= 0 && $MIN_SURVEY_REQUIREMENT == 0){
 		$allUsers = User::findAll();
 		for($j = 0; $j < count($allUsers); $j++){
 			if(is_object($allUsers[$j]) && get_class($allUsers[$j]) == "User" && !array_key_exists(strval($allUsers[$j]->getID()), $rankingsArray)){
