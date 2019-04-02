23b8e5f1287939f9bc14c70985ffa5b724649362
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-04-02 03:39:18 -0400

    add min survey requirement

--- php/getSiteRankings.php
+++ php/getSiteRankings.php
@@ -5,6 +5,8 @@
 	$HIGH_TRAFFIC_MODE = true;
 	$SAVE_TIME_LIMIT = 60;
 	
+	$MIN_SURVEY_REQUIREMENT = 10;
+	
 	$baseFileName = basename(__FILE__, '.php');
 	if($HIGH_TRAFFIC_MODE){
 		$save = getSave(basename(__FILE__, '.php'), $SAVE_TIME_LIMIT);
@@ -14,7 +16,7 @@
 	}
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
-	$query = mysqli_query($dbconn, "SELECT Site.ID, Site.Name, Site.Region, Site.Latitude, Site.Longitude, Site.OpenToPublic, SUM(CASE WHEN Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) THEN 1 ELSE 0 END) AS Week, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Month, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Year, Count(*) AS Total, COUNT(DISTINCT Survey.LocalDate) AS TotalUniqueDates FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Site.ID<>2 GROUP BY Site.ID ORDER BY Year DESC");
+	$query = mysqli_query($dbconn, "SELECT Site.ID, Site.Name, Site.Region, Site.Latitude, Site.Longitude, Site.OpenToPublic, SUM(CASE WHEN Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) THEN 1 ELSE 0 END) AS Week, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Month, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Year, Count(*) AS Total, COUNT(DISTINCT Survey.LocalDate) AS TotalUniqueDates FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Site.ID<>2 AND Total>='$MIN_SURVEY_REQUIREMENT' GROUP BY Site.ID ORDER BY Year DESC");
 		
 	$rankingsArray = array();
   	$i = 1;
@@ -36,47 +38,49 @@
     		);
 	}
 	
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisWeek FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>2 AND Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) GROUP BY Plant.SiteFK");
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisWeek FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK<>2 AND Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) GROUP BY Plant.SiteFK");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["SiteFK"])]["UniqueDatesThisWeek"] = intval($row["UniqueDatesThisWeek"]);
 	}
 
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisMonth FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>2 AND Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') GROUP BY Plant.SiteFK");
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisMonth FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK<>2 AND Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') GROUP BY Plant.SiteFK");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["SiteFK"])]["UniqueDatesThisMonth"] = intval($row["UniqueDatesThisMonth"]);
 	}
 
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>2 AND Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') GROUP BY Plant.SiteFK");
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT LocalDate) AS UniqueDatesThisYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK<>2 AND Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') GROUP BY Plant.SiteFK");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["SiteFK"])]["UniqueDatesThisYear"] = intval($row["UniqueDatesThisYear"]);
 	}
 
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS Caterpillars FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>2 AND ArthropodSighting.Group='caterpillar' GROUP BY Plant.SiteFK");
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS Caterpillars FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK IN (0, " . implode(", ", array_keys($rankingsArray)) . ") AND Plant.SiteFK<>2 AND ArthropodSighting.Group='caterpillar' GROUP BY Plant.SiteFK");
 	while($row = mysqli_fetch_assoc($query)){
 		$rankingsArray[strval($row["SiteFK"])]["Caterpillars"] = round(((floatval($row["Caterpillars"]) / floatval($rankingsArray[strval($row["SiteFK"])]["Total"])) * 100), 2) . "%";
 	}
 	
-	$query = mysqli_query($dbconn, "SELECT `ID`, `OpenToPublic`, `Latitude`, `Longitude`, `Name`, `Region` FROM `Site`");
-	while($row = mysqli_fetch_assoc($query)){
-		if(intval($row["ID"]) != 2 && !array_key_exists(strval($row["ID"]), $rankingsArray)){
-			$coordinates = "NONE";
-			if($row["OpenToPublic"]){
-				$coordinates = $row["Latitude"] . "," . $row["Longitude"];
+	if($MIN_SURVEY_REQUIREMENT == 0){
+		$query = mysqli_query($dbconn, "SELECT `ID`, `OpenToPublic`, `Latitude`, `Longitude`, `Name`, `Region` FROM `Site`");
+		while($row = mysqli_fetch_assoc($query)){
+			if(intval($row["ID"]) != 2 && !array_key_exists(strval($row["ID"]), $rankingsArray)){
+				$coordinates = "NONE";
+				if($row["OpenToPublic"]){
+					$coordinates = $row["Latitude"] . "," . $row["Longitude"];
+				}
+				$rankingsArray[strval($row["ID"])] = array(
+					"ID" => $row["ID"],
+					"Name" => $row["Name"] . " (" . $row["Region"] . ")",
+					"Coordinates" => $coordinates,
+					"Week" => 0,
+					"UniqueDatesThisWeek" => 0,
+					"Month" => 0,
+					"UniqueDatesThisMonth" => 0,
+					"Year" => 0,
+					"UniqueDatesThisYear" => 0,
+					"Total" => 0,
+					"TotalUniqueDates" => 0,
+					"Caterpillars" => "0%",
+				);
 			}
-			$rankingsArray[strval($row["ID"])] = array(
-				"ID" => $row["ID"],
-				"Name" => $row["Name"] . " (" . $row["Region"] . ")",
-				"Coordinates" => $coordinates,
-				"Week" => 0,
-				"UniqueDatesThisWeek" => 0,
-				"Month" => 0,
-				"UniqueDatesThisMonth" => 0,
-				"Year" => 0,
-				"UniqueDatesThisYear" => 0,
-				"Total" => 0,
-      				"TotalUniqueDates" => 0,
-				"Caterpillars" => "0%",
-			);
 		}
 	}
 	mysqli_close($dbconn);
