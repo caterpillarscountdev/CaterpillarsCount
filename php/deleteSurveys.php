c34d52a6b7c65538536d4226a562063d7bbf92b9
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-04-22 18:55:16 -0400

    remove $failures

--- php/deleteSurveys.php
+++ php/deleteSurveys.php
@@ -10,9 +10,6 @@
 
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-		$successes = 0;
-		$failures = 0;
-		
 		$userSites = $user->getSites();
 		$userSiteIDs = array();
 		for($i = 0; $i < count($userSites); $i++){
@@ -36,17 +33,17 @@
 		mysqli_query($dbconn, "DELETE ArthropodSighting FROM ArthropodSighting JOIN Survey on ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Survey.ID IN (-1, " . join(", ", $selected) . ") AND (Site.ID IN (-1, " . join(", ", $userSiteIDs) . ") OR (Survey.UserFKOfObserver='" . $user->getID() . "' AND Survey.SubmissionTimestamp>='" . (time() - (2 * 7 * 24 * 60 * 60)) . "'))");
 		mysqli_query($dbconn, "DELETE Survey FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Survey.ID IN (-1, " . join(", ", $selected) . ") AND (Site.ID IN (-1, " . join(", ", $userSiteIDs) . ") OR (Survey.UserFKOfObserver='" . $user->getID() . "' AND Survey.SubmissionTimestamp>='" . (time() - (2 * 7 * 24 * 60 * 60)) . "'))");
 		$successes = mysqli_affected_rows($dbconn);
-		$failures = intval(mysqli_fetch_assoc(mysqli_query($dbconn, "SELECT COUNT(*) AS `Count` FROM Survey WHERE Survey.ID IN (-1, " . join(", ", $selected) . ")"))["Count"]);
+		$tryingToDeleteCount = (count($selected) - count($unselected));
 		
-		if($failures > 0){
-			if(count($selected) < 2){
+		if($successes < $tryingToDeleteCount){
+			if(count($tryingToDeleteCount) < 2){
 				die("false|You do not have the authority to delete this survey.");
 			}
 			else if($successes == 1){
-				die("false|Only " . $successes . " survey was successfully deleted. You do not have the authority to delete " . $failures . " of the surveys you tried to delete.");
+				die("false|Only " . $successes . " survey was successfully deleted. You do not have the authority to delete " . ($tryingToDeleteCount - $successes) . " of the surveys you tried to delete.");
 			}
 			else if($successes > 1){
-				die("false|Only " . $successes . " surveys were successfully deleted. You do not have the authority to delete " . $failures . " of the surveys you tried to delete.");
+				die("false|Only " . $successes . " surveys were successfully deleted. You do not have the authority to delete " . ($tryingToDeleteCount - $successes) . " of the surveys you tried to delete.");
 			}
 		}
 		die("true|" . $successes);
