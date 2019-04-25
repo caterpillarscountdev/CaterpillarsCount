eb2b5ebf6c7b7d97b7636e385907a5fcdcabce46
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-04-25 05:07:17 -0400

    add last plants to sites

--- php/forcePlantCreation.php
+++ php/forcePlantCreation.php
@@ -1,28 +1,13 @@
 <?php
   require_once('orm/resources/Keychain.php');
-  
-  $i = intval(file_get_contents("iteration.txt"));
-  
-  $plants = array(array('5713', '178', '4', 'A', '3541', 'Red maple'),
-array('5714', '178', '4', 'B', '3542', 'Red oak'),
-array('5715', '178', '4', 'C', '3543', 'Red oak'),
-array('5716', '178', '4', 'D', '3544', 'American chestnut'),
-array('5717', '178', '4', 'E', '3545', 'American chestnut'),
-array('5718', '178', '5', 'A', '3546', 'American chestnut'),
-array('5719', '178', '5', 'B', '3547', 'American chestnut'),
-array('5720', '178', '5', 'C', '3548', 'Red maple'),
-array('5721', '178', '5', 'D', '3549', 'Red oak'),
-array('5722', '178', '5', 'E', '3550', 'Red oak'));
 
-	$dbconn = (new Keychain)->getDatabaseConnection();
-  	for($j = 0; $j < 10; $j++){
-    		$id = (1000003540 + $i + $j);
-    		mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('" . $id . "', '" . $plants[$i + $j][1] . "', '" . $plants[$i + $j][2] . "', '" . $plants[$i + $j][3] . "', '" . IDToCode($id) . "', '" . $plants[$i + $j][5] . "')");
-	}
-  
-	$myfile = fopen("iteration.txt", "w");
-	fwrite($myfile, (($i + 20) . ""));
-	fclose($myfile);
+$dbconn = (new Keychain)->getDatabaseConnection();
+
+mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('1000003550', '176', '260', 'D', '" . IDToCode(1000003550) . "', 'N/A')");
+mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('1000003551', '176', '260', 'E', '" . IDToCode(1000003551) . "', 'N/A')");
+mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('1000003552', '177', '336', 'C', '" . IDToCode(1000003552) . "', 'N/A')");
+mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('1000003553', '177', '336', 'D', '" . IDToCode(1000003553) . "', 'N/A')");
+mysqli_query($dbconn, "INSERT INTO Plant (`ID`, `SiteFK`, `Circle`, `Orientation`, `Code`, `Species`) VALUES ('1000003554', '177', '336', 'E', '" . IDToCode(1000003554) . "', 'N/A')");
   
   
   function IDToCode($id){
