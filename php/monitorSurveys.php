857d63857a4a9e9e29924d77614ad90ad1f7dbe6
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-11 15:22:15 -0400

    allow survey 94475

--- php/monitorSurveys.php
+++ php/monitorSurveys.php
@@ -4,7 +4,7 @@
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	
-	$query = mysqli_query($dbconn, "SELECT Survey.ID FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID NOT IN ('719', '2985', '2986', '2987', '2988', '2989', '2990', '2991', '2992', '2993', '2994', '2995', '2996', '2997', '2998', '2999', '3000', '5487', '6202', '6701', '11555', '28828', '29079', '29085', '29357', '29504', '29938', '30315', '30799', '31068') GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY `Survey`.`SubmissionTimestamp` ASC");
+	$query = mysqli_query($dbconn, "SELECT Survey.ID FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID NOT IN ('719', '2985', '2986', '2987', '2988', '2989', '2990', '2991', '2992', '2993', '2994', '2995', '2996', '2997', '2998', '2999', '3000', '5487', '6202', '6701', '11555', '28828', '29079', '29085', '29357', '29504', '29938', '30315', '30799', '31068', '94475') GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY `Survey`.`SubmissionTimestamp` ASC");
 	$ids = array();
   if(mysqli_num_rows($query) > 0){
 		while($row = mysqli_fetch_assoc($query)){
