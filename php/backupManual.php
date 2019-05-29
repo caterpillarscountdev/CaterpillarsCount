99404c511ae172a939afd76b8298be7ab7a240e1
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-29 17:47:11 -0400

    add port

--- php/backupManual.php
+++ php/backupManual.php
@@ -6,6 +6,6 @@
   ini_set('memory_limit', '-1');
   
   //$result = exec('mysqldump --skip-lock-tables -h ' . getenv("CATERPILLARSV2_SERVICE_HOST") . ' -P ' . getenv("CATERPILLARSV2_SERVICE_PORT") . ' \-u ' . getenv("HOST_USERNAME") . ' --password="' . getenv("HOST_PASSWORD") . '" --all-databases > ../backupsManual/' . date("Y-m-d") . '.sql 2>&1', $output);
-  exec("mysqldump --user={" . getenv("HOST_USERNAME") . "} --password={" . getenv("HOST_PASSWORD") . "} --host={" . getenv("CATERPILLARSV2_SERVICE_HOST") . "} {" . getenv("DATABASE_NAME") . "} --result-file={../backupsManual/" . date("Y-m-d") . ".sql} 2>&1", $output);
+  exec("mysqldump --user={" . getenv("HOST_USERNAME") . "} --password={" . getenv("HOST_PASSWORD") . "} --host={" . getenv("CATERPILLARSV2_SERVICE_HOST") . "} --port={" . getenv("CATERPILLARSV2_SERVICE_PORT") . "} {" . getenv("DATABASE_NAME") . "} --result-file={../backupsManual/" . date("Y-m-d") . ".sql} 2>&1", $output);
   var_dump($output);
 ?>
