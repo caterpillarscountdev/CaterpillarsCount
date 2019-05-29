bd5153f5525f95acac9e7d5553b3eec10dfbe3a3
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-29 16:45:14 -0400

    root pass

--- php/backupManual.php
+++ php/backupManual.php
@@ -5,7 +5,7 @@
   
   ini_set('memory_limit', '-1');
   
-  $result = exec("mysqldump --user=" . getenv("HOST_USERNAME") . " --password=" . getenv("HOST_PASSWORD") . " --host=" . getenv("CATERPILLARSV2_SERVICE_HOST") . " " . getenv("DATABASE_NAME") . " > ../backupsManual/" . date("Y-m-d") . ".sql", $output);
+  $result = exec("mysqldump --user=" . getenv("HOST_USERNAME") . " --password=" . getenv("DATABASE_ROOT_PASSWORD") . " --host=" . getenv("CATERPILLARSV2_SERVICE_HOST") . " " . getenv("DATABASE_NAME") . " > ../backupsManual/" . date("Y-m-d") . ".sql", $output);
   if($output==''){/* no output is good */}
   else {/* we have something to log the output here*/}
   echo $tableName . ": [" . var_dump($output) . "]";
