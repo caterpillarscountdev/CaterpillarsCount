5df61e30df15edb93562f88c698e4d54ed9bfa27
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-29 16:14:39 -0400

    user is root

--- php/backupManual.php
+++ php/backupManual.php
@@ -5,7 +5,7 @@
   
   ini_set('memory_limit', '-1');
   
-  $result = exec("mysqldump " . getenv("DATABASE_NAME") . " --password=" . getenv("HOST_PASSWORD") . " --user=" . getenv("HOST_USERNAME") . " --single-transaction >../backupsManual/" . date("Y-m-d") . ".sql", $output);
+  $result = exec("mysqldump " . getenv("DATABASE_NAME") . " --password=" . getenv("HOST_PASSWORD") . " --user=root --single-transaction >../backupsManual/" . date("Y-m-d") . ".sql", $output);
   if($output==''){/* no output is good */}
   else {/* we have something to log the output here*/}
   echo $tableName . ": [" . var_dump($output) . "]";
