8952d7ad0c9d77df5f704e5717508dcbb947a392
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-29 15:47:15 -0400

    mysqldump

--- php/backupManual.php
+++ php/backupManual.php
@@ -2,27 +2,21 @@
   header('Access-Control-Allow-Origin: *');
   
   require_once('orm/resources/Keychain.php');
+  
   ini_set('memory_limit', '-1');
+  
   function createCSV($tableName) {
-    $dbconn = (new Keychain)->getDatabaseConnection();
-    
-    $headerSQLString = "";
-    $query = mysqli_query($dbconn, "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='CaterpillarsCount' AND `TABLE_NAME`='" . $tableName . "'");
-    $colHeaders = array();
-    while ($row = mysqli_fetch_assoc($query)) $colHeaders[] = $row["COLUMN_NAME"];
-    if(count($colHeaders) > 0){
-      $headerSQLString = "SELECT " . implode(", ", $colHeaders) . " UNION ALL ";
-    }
-    
     $directory = "backupsManual";
     if($tableName == "User"){
       return false;
       $directory = getenv("USER_BACKUPS");
     }
-    
-    mysqli_query($dbconn, $headerSQLString . "SELECT * INTO OUTFILE '../" . $directory . "/" . date("Y-m-d") . "_" . $tableName . ".csv' FROM `" . $tableName . "`");
-    mysqli_close($dbconn);
+    $result = exec("mysqldump " . getenv("DATABASE_NAME") . " --password=" . getenv("HOST_PASSWORD") . " --user=" . getenv("HOST_USERNAME") . " --single-transaction >../" . $directory . "/" . date("Y-m-d") . "_" . $tableName . ".csv", $output);
+    if($output==''){/* no output is good */}
+    else {/* we have something to log the output here*/}
+    echo $tableName . ": [" . $output . "]";
   }
+  
   //backup
   $dbconn = (new Keychain)->getDatabaseConnection();
   $query = mysqli_query($dbconn, "SELECT table_name FROM information_schema.tables WHERE table_schema='CaterpillarsCount' AND table_name<>'TemporaryEmailLog'");
