dd04250ca05729195ee1eefc440ed51eea83de84
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-05-29 18:12:56 -0400

    Update backupManual.php

--- php/backupManual.php
+++ php/backupManual.php
@@ -1,4 +1,4 @@
 <?php
-  exec("mysqldump --user={" . getenv("HOST_USERNAME") . "} --password={" . getenv("HOST_PASSWORD") . "} --host={" . getenv("CATERPILLARSV2_SERVICE_HOST") . "} --port={" . getenv("CATERPILLARSV2_SERVICE_PORT") . "} {" . getenv("DATABASE_NAME") . "} > {../backupsManual/" . date("Y-m-d") . ".sql} 2>&1", $output);
+  exec("mysqldump --user={" . getenv("HOST_USERNAME") . "} --password={" . getenv("HOST_PASSWORD") . "} --host={" . getenv("CATERPILLARSV2_SERVICE_HOST") . "} --port={" . getenv("CATERPILLARSV2_SERVICE_PORT") . "} {" . getenv("DATABASE_NAME") . "} --result-file={../backupsManual/" . date("Y-m-d") . ".sql} 2>&1", $output);
   var_dump($output);
 ?>
