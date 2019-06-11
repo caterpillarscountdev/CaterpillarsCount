5b2375c679bc2e3ebda3563049c2347fbc789f35
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-06-10 23:15:22 -0400

    Update backup.php

--- php/backup.php
+++ php/backup.php
@@ -1,49 +1,105 @@
 <?php
   header('Access-Control-Allow-Origin: *');
   
+	require_once('resultMemory.php');
   require_once('orm/resources/Keychain.php');
+	ini_set('memory_limit', '-1');
   
-  ini_set('memory_limit', '-1');
-  
-  function createCSV($tableName) {
+  function getArrayFromTable($tableName){
+    $CHUNK_SIZE = 20000;
+    $baseFileName = $tableName . basename(__FILE__, '.php');
+    
+    $tableArray = array();
     $dbconn = (new Keychain)->getDatabaseConnection();
     
-    $headerSQLString = "";
+    //HEADERS
     $query = mysqli_query($dbconn, "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='CaterpillarsCount' AND `TABLE_NAME`='" . $tableName . "'");
     $colHeaders = array();
     while ($row = mysqli_fetch_assoc($query)) $colHeaders[] = $row["COLUMN_NAME"];
-    if(count($colHeaders) > 0){
-      $headerSQLString = "SELECT " . implode(", ", $colHeaders) . " UNION ALL ";
+    $tableArray[] = $colHeaders;
+    
+    $iteration = 0;
+    $save = getSave($baseFileName, 60 * 60);
+    if($save !== null){
+      $iteration = substr($save, 0, strpos($save, ";"));
+      $tableArray = json_decode(substr($save, strpos($save, ";") + 1));
     }
     
+    //ROWS
+    $query = mysqli_query($dbconn, "SELECT COUNT(*) AS Count FROM `" . $tableName . "`");
+    $rowCount = mysqli_fetch_assoc($query)["Count"];
+    
+    if($rowCount > (count($tableArray) - 1)){
+      $query = mysqli_query($dbconn, "SELECT * FROM `" . $tableName . "` LIMIT " . $iteration * $CHUNK_SIZE . ", " . $CHUNK_SIZE);
+      mysqli_close($dbconn);
+      while ($row = mysqli_fetch_assoc($query)){
+        $rowArray = array();
+        for($i = 0; $i < count($colHeaders); $i++){
+          $rowArray[] = $row[$colHeaders[$i]];
+        }
+        $tableArray[] = $rowArray;
+      }
+    }
+    
+    if($rowCount > ((++$iteration) * $CHUNK_SIZE)){
+      save($baseFileName, $iteration . ";" . json_encode($tableArray));
+      save("allBackedUp", "false");
+      return false;
+    }
+    
+    return $tableArray;
+  }
+  function createCSV($tableName, $tableArray) {
     $directory = "backups";
     if($tableName == "User"){
       $directory = getenv("USER_BACKUPS");
     }
-    
-    mysqli_query($dbconn, $headerSQLString . "SELECT * INTO OUTFILE '/opt/app-root/src/" . $directory . "/" . date("Y-m-d") . "_" . $tableName . ".csv' FROM `" . $tableName . "`");
-    mysqli_close($dbconn);
+    if(!$fp = fopen("/opt/app-root/src/" . $directory . "/" . date("Y-m-d") . "_" . $tableName . ".csv", 'w')) return false;
+    foreach ($tableArray as $line) fputcsv($fp, $line);
   }
   function backup($tableName){
-    createCSV($tableName);
-  }
-  
-  $files = scandir("/opt/app-root/src/backups");
-  $backedUpToday = false;
-  for($i = 0; $i < count($files); $i++){
-    if(strpos($files[$i], date("Y-m-d")) !== false){
-      $backedUpToday = true;
+    if(!hasBeenBackedUpToday($tableName)){
+      $tableArray = getArrayFromTable($tableName);
+      if($tableArray !== false){
+        createCSV($tableName, $tableArray);
+      }
     }
   }
-  if(!$backedUpToday){
-    //backup
+  $files = scandir("/opt/app-root/src/backups");
+  function hasBeenBackedUpToday($tableName){
+    global $files;
+    for($i = 0; $i < count($files); $i++){
+      if($files[$i] == (date("Y-m-d") . "_" . $tableName)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function haveDeletedOldFiles(){
+    global $files;
+    for($i = 0; $i < count($files); $i++){
+      if(strpos($files[$i], date("Y-m-d") . "_") !== false){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+	//backup
+	
+  if(getSave("allBackedUp", 100 * 365 * 24 * 60 * 60) != date("Y-m-d")){
+    save("allBackedUp", date("Y-m-d"));
     $dbconn = (new Keychain)->getDatabaseConnection();
     $query = mysqli_query($dbconn, "SELECT table_name FROM information_schema.tables WHERE table_schema='CaterpillarsCount' AND table_name<>'TemporaryEmailLog'");
     mysqli_close($dbconn);  
     while($row = mysqli_fetch_assoc($query)){
       backup($row["table_name"]);
     }
-    
+  }
+  
+  if(!haveDeletedOldFiles()){
     //delete older files
     $acceptableDates = array(
       date("Y-m-d"), //today
@@ -54,7 +110,6 @@
       date("Y-m-d", time() - 60 * 60 * 24 * 5),
       date("Y-m-d", time() - 60 * 60 * 24 * 6),
     );
-    
     for($i = 0; $i < count($files); $i++){
       $dateIsAcceptable = false;
       for($j = 0; $j < count($acceptableDates); $j++){
@@ -62,12 +117,10 @@
           $dateIsAcceptable = true;
         }
       }
-      
       if(!$dateIsAcceptable && strlen(str_replace(".", "", $files[$i])) > 0){
         unlink("/opt/app-root/src/backups/" . $files[$i]);
       }
     }
-    
     //delete from user backups too
     $files = scandir("/opt/app-root/src/" . getenv("USER_BACKUPS"));
     for($i = 0; $i < count($files); $i++){
@@ -77,7 +130,6 @@
           $dateIsAcceptable = true;
         }
       }
-      
       if(!$dateIsAcceptable && strlen(str_replace(".", "", $files[$i])) > 0){
         unlink("/opt/app-root/src/" . getenv("USER_BACKUPS") . "/" . $files[$i]);
       }
