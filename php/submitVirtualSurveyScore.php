3531f16ed43610ec7fdd20577a7f4f287698a5d6
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-06-26 01:16:45 -0400

    fix indentation

--- php/submitVirtualSurveyScore.php
+++ php/submitVirtualSurveyScore.php
@@ -1,17 +1,16 @@
 <?php
-	require_once('orm/resources/Keychain.php');
 	require_once('orm/User.php');
   
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
 	$score = intval($_GET["score"]);
   
-  $user = User::findBySignInKey($email, $salt);
+  	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    		if($user->submitVirtualSurveyScore($score)){
-    		  die("true|");
-        }
-        die("false|We could not submit that score.");
+		if($user->submitVirtualSurveyScore($score)){
+			die("true|");
+        	}
+        	die("false|We could not submit that score.");
   	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
