7c52c074bca3145d4282a12348b19779ad0676d0
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-06-26 01:11:24 -0400

    Create submitVirtualSurveyScore.php

---
+++ php/submitVirtualSurveyScore.php
@@ -0,0 +1,17 @@
+<?php
+	require_once('orm/resources/Keychain.php');
+	require_once('orm/User.php');
+  
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	$score = intval($_GET["score"]);
+  
+  $user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    		if($user->submitVirtualSurveyScore($score)){
+    		  die("true|");
+        }
+        die("false|We could not submit that score.");
+  	}
+	die("false|Your log in dissolved. Maybe you logged in on another device.");
+?>
