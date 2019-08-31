f06d648a754338548fe08e2140e6a2953ac2f140
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-08-31 02:23:50 -0400

    Update findDuplicatePhotos.php

--- php/findDuplicatePhotos.php
+++ php/findDuplicatePhotos.php
@@ -1,10 +1,15 @@
 <?php
+  $limit = intval($_GET["limit"]);
   $photos = scandir("../images/arthropods");
   for($i = 0; $i < count($photos); $i++){
+    if($i >= $limit){
+      break;
+    }
+    
     //echo "checking...";
     $photoHash = hash_file("md5", "../images/arthropods/" . $photos[$i]);
     for($j = 0; $j < count($photos); $j++){
-      if(hash_file("md5", "../images/arthropods/" . $photos[$j]) == $photoHash){
+      if($i != $j && hash_file("md5", "../images/arthropods/" . $photos[$j]) == $photoHash){
         echo $photos[$i];
         break;
       }
