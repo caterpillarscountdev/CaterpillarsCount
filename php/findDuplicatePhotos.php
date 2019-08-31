b9d24f8eb6de504d3a676202c29318ef0c219968
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-08-31 02:16:56 -0400

    count(), not .length

--- php/findDuplicatePhotos.php
+++ php/findDuplicatePhotos.php
@@ -1,9 +1,9 @@
 <?php
   $photos = scandir("../images/arthropods");
-  for($i = 0; $i < $photos.length; $i++){
-    echo "checking...";
+  for($i = 0; $i < count($photos); $i++){
+    //echo "checking...";
     $photoHash = hash_file("md5", "../images/arthropods/" . $photos[$i]);
-    for($j = 0; $j < $photos.length; $j++){
+    for($j = 0; $j < count($photos); $j++){
       if($i != $j && hash_file("md5", "../images/arthropods/" . $photos[$j]) == $photoHash){
         echo $photos[$i];
         break;
