85277c1ff0d5823c323a7840081914b50eb9ba1b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-09-02 00:25:03 -0400

    Update findDuplicatePhotos.php

--- php/findDuplicatePhotos.php
+++ php/findDuplicatePhotos.php
@@ -2,6 +2,7 @@
   $offset = intval($_GET["offset"]);
   $limit = intval($_GET["limit"]);
   
+  $usedPhotoHashes = array();
   $photoHashes = array();
   $photos = scandir("../images/arthropods");
   for($i = 0; $i < count($photos); $i++){
@@ -9,11 +10,16 @@
   }
 
   for($i = 0; $i < count($photoHashes); $i++){
+    $isDuplicate = false;
     for($j = 0; $j < count($photoHashes); $j++){
-      if($i != $j && $photoHashes[$i] == $photoHashes[$j]){
-        echo $photos[$i];
-        break;
+      if($i != $j && !in_array($photoHashes[$photos[$i]], $usedPhotoHashes) && $photoHashes[$photos[$i]] == $photoHashes[$photos[$j]]){
+        $isDuplicate = true;
+        echo "<a href=\"https://caterpillarscount.unc.edu/images/arthropods/" . $photos[$i] . "\" target=\"_blank\">" . $photos[$i] . "</a><br/>";
       }
+    }
+    if($isDuplicate){
+      $usedPhotoHashes[] = $photoHashes[$photos[$i]];
+      echo "<br/>";
     }
   }
 ?>
