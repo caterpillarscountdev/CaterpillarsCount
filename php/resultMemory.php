699db36303e5ddfe729d660ef70f01a16807790c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-09-25 16:33:00 -0400

    add database compatibility

--- php/resultMemory.php
+++ php/resultMemory.php
@@ -1,4 +1,6 @@
 <?php
+	require_once("orm/resources/Keychain.php");
+	
 	function save($baseFileName, $result){
 		$saveFilePath = $baseFileName . ".txt";
 		$saveFile = fopen($saveFilePath, "w");
@@ -15,6 +17,38 @@
 				if(time() - $savedTime <= $timeLimit){
 					return substr($contents, (strpos($contents, "|") + 1));
 				}
+			}
+		}
+		return null;
+	}
+
+	function saveInDatabase($baseFileName, $result){
+		$dbconn = (new Keychain)->getDatabaseConnection();
+		$query = mysqli_query($dbconn, "SELECT * FROM CachedResult WHERE `Name`='$baseFileName' LIMIT 1");
+		
+		$result = mysqli_real_escape_string($dbconn, $result);
+		if(mysqli_num_rows($query) <= 0){
+			//insert
+			mysqli_query($dbconn, "INSERT INTO `CachedResult` (`Name`, `Timestamp`, `Result`) VALUES ('$baseFileName', '" . time() . "', '$result')");
+		}
+		else{
+			//update
+			mysqli_query($dbconn, "UPDATE `CachedResult` SET `Timestamp`='" . time() . "', `Result`='$result' WHERE `Name`='$baseFileName'");
+		}
+		
+		mysqli_close($dbconn);
+	}
+  
+	function getSaveFromDatabase($baseFileName, $timeLimit){
+		$dbconn = (new Keychain)->getDatabaseConnection();
+		$query = mysqli_query($dbconn, "SELECT * FROM CachedResult WHERE `Name`='$baseFileName' LIMIT 1");
+		mysqli_close($dbconn);
+		
+		if(mysqli_num_rows($query) > 0){
+			$row = mysqli_fetch_assoc($query);
+			$savedTime = intval($row["Timestamp"]);
+			if(time() - $savedTime <= $timeLimit){
+				return $row["Result"];
 			}
 		}
 		return null;
