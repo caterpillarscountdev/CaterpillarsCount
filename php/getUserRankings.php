f363e89b6c8bbf61ec3d74d86e96643d1bb2950c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-09-25 20:49:42 -0400

    add cron default

--- php/getUserRankings.php
+++ php/getUserRankings.php
@@ -4,9 +4,9 @@
 	require_once('resultMemory.php');
 	
 	$siteID = intval($_GET["siteID"]);
-	$forceSave = false;
-	if(isset($_GET["forceSave"]) && !empty($_GET["forceSave"])){
-		$forceSave = filter_var($_GET["forceSave"], FILTER_VALIDATE_BOOLEAN);
+	$cron = true;
+	if(isset($_GET["cron"]) && !empty($_GET["cron"])){
+		$cron = filter_var($_GET["cron"], FILTER_VALIDATE_BOOLEAN);
 	}
 
 	$HIGH_TRAFFIC_MODE = true;
@@ -15,7 +15,7 @@
 	$MIN_SURVEY_REQUIREMENT = 10;//only for the global leaderboard- doesn't apply to the site-specific leaderboard
 	
 	$baseFileName = basename(__FILE__, '.php') . $siteID;
-	if($HIGH_TRAFFIC_MODE && !$forceSave){
+	if($HIGH_TRAFFIC_MODE && !$cron){
 		$save = getSaveFromDatabase($baseFileName, $SAVE_TIME_LIMIT);
 		if($save !== null){
 			die($save);
