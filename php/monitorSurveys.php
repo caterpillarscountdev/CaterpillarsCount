255d3ee4700b4dc1603feca89c441990d0d4286a
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-11-16 10:36:16 -0500

    email more data

--- php/monitorSurveys.php
+++ php/monitorSurveys.php
@@ -1,18 +1,26 @@
 <?php
-  require_once('/opt/app-root/src/php/orm/resources/Keychain.php');
-  require_once('/opt/app-root/src/php/orm/resources/mailing.php');
+  	require_once('/opt/app-root/src/php/orm/resources/Keychain.php');
+  	require_once('/opt/app-root/src/php/orm/resources/mailing.php');
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	
-	$query = mysqli_query($dbconn, "SELECT Survey.ID FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID NOT IN ('719', '2985', '2986', '2987', '2988', '2989', '2990', '2991', '2992', '2993', '2994', '2995', '2996', '2997', '2998', '2999', '3000', '5487', '6202', '6701', '11555', '28828', '29079', '29085', '29357', '98116', '29504', '29938', '30315', '30799', '31068', '94475', '95976', '95989') GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY `Survey`.`SubmissionTimestamp` ASC");
-	$ids = array();
-  if(mysqli_num_rows($query) > 0){
+	$mobileTimeouts = array();
+	$query = mysqli_query($dbconn, "SELECT Survey.ID, Plant.Code, Survey.LocalDate, Survey.LocalTime, CONCAT(User.FirstName, " ", User.LastName) AS FullName FROM Survey JOIN User ON Survey.UserFKOfObserver = User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Survey.SubmittedThroughApp='1' AND Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID > '100000' GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY Survey.SubmittedThroughApp DESC, `Survey`.`SubmissionTimestamp` ASC")
+	if(mysqli_num_rows($query) > 0){
 		while($row = mysqli_fetch_assoc($query)){
-      $ids[] = $row["ID"];
-	  }
+			$mobileTimeouts[] = "SURVEY ID: " . $row["ID"] . "| " . $row["Code"] . " " . $row["LocalDate"] . " " . $row["LocalTime"] . " " . $row["FullName"];
+		}
+	}
+	
+	$desktopResubmissions = array();
+	$query = mysqli_query($dbconn, "SELECT Survey.ID, Plant.Code, Survey.LocalDate, Survey.LocalTime, CONCAT(User.FirstName, " ", User.LastName) AS FullName FROM Survey JOIN User ON Survey.UserFKOfObserver = User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Survey.SubmittedThroughApp='0' AND Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID > '100000' GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY Survey.SubmittedThroughApp DESC, `Survey`.`SubmissionTimestamp` ASC")
+	if(mysqli_num_rows($query) > 0){
+		while($row = mysqli_fetch_assoc($query)){
+			$desktopResubmissions[] = "SURVEY ID: " . $row["ID"] . "| " . $row["Code"] . " " . $row["LocalDate"] . " " . $row["LocalTime"] . " " . $row["FullName"];
+		}
 	}
   
-  if(count($ids) > 0){
-    email("plocharczykweb@gmail.com", "Duplicate survey detected", "There may have been a timeout or some other error that caused a user to have to submit the same survey twice. The following Survey IDs should be investigated: " . implode(", ", $ids));
-  }
+  	if(count($mobileTimeouts) > 0 || count($desktopResubmissions) > 0){
+		email("plocharczykweb@gmail.com", "Duplicate survey detected", "There may have been a timeout or some other error that caused a user to have to submit the same survey twice. The following Survey IDs should be investigated: \n\n\nMOBILE APP:\n" . implode("\n\n", $mobileTimeouts)) . "\n\n\nWEBSITE:\n" . implode("\n\n", $desktopResubmissions));
+  	}
 ?>
