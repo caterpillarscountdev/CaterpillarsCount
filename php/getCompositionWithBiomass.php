db598976072b861e651ace3192bedf9c58e90dea
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-11-16 15:05:16 -0500

    add mean biomass data

--- php/getCompositionWithBiomass.php
+++ php/getCompositionWithBiomass.php
@@ -1,10 +1,11 @@
 <?php
  	require_once('orm/resources/Keychain.php');
 	require_once('tools/resultMemory.php');
+	require_once('tools/biomassCalculator.php');
    
  	$siteIDs = json_decode($_GET["siteIDs"]);
  	$breakdown = $_GET["breakdown"]; //site, year, plant species, none
- 	$comparisonMetric = $_GET["comparisonMetric"]; //occurrence, absoluteDensity, relativeProportion
+ 	$comparisonMetric = $_GET["comparisonMetric"]; //occurrence, absoluteDensity, relativeProportion, meanBiomass
 	$HIGH_TRAFFIC_MODE = true;
 	$SAVE_TIME_LIMIT = 60;
    
@@ -51,12 +52,12 @@
  			if($comparisonMetric == "occurrence"){
 				//surveys with arthropod at site
 				$arthropodSurveys = array();
-				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS SurveysWithArthropodCount FROM `ArthropodSighting` JOIN Survey ON ArthropodSighting.SurveyFK = Survey.ID JOIN Plant ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK = '$siteID'" . $extraSQL . " GROUP BY ArthropodSighting.Group");
+				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, COUNT(DISTINCT ArthropodSighting.SurveyFK) AS SurveysWithArthropodCount FROM `ArthropodSighting` JOIN Survey ON ArthropodSighting.SurveyFK = Survey.ID JOIN Plant ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK = '$siteID' GROUP BY ArthropodSighting.Group");
 				while($row = mysqli_fetch_assoc($query)){
 					$arthropodSurveys[$row["Group"]] = floatval($row["SurveysWithArthropodCount"]);
 				}
 				//surveys at site
-				$query = mysqli_query($dbconn, "SELECT COUNT(*) AS TotalSurveyCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'" . $extraSQL);
+				$query = mysqli_query($dbconn, "SELECT COUNT(*) AS TotalSurveyCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'");
 				$totalSurveyCount = floatval(mysqli_fetch_assoc($query)["TotalSurveyCount"]);
 				$arthropodOccurrence = array();
 				$keys = array_keys($arthropodSurveys);
@@ -72,17 +73,17 @@
  			else if($comparisonMetric == "absoluteDensity"){
 				//sum of each arthropod at site
 				$arthropodCounts = array();
-				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'" . $extraSQL . " GROUP BY ArthropodSighting.Group");
+				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' GROUP BY ArthropodSighting.Group");
 				while($row = mysqli_fetch_assoc($query)){
 					$arthropodCounts[$row["Group"]] = floatval($row["ArthropodCount"]);
 				}
 				//total survey count at site
-				$query = mysqli_query($dbconn, "SELECT COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'" . $extraSQL);
+				$query = mysqli_query($dbconn, "SELECT COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'");
 				$surveyCount = floatval(mysqli_fetch_assoc($query)["SurveyCount"]);
 				$arthropodAbsoluteDensity = array();
 				$keys = array_keys($arthropodCounts);
 				for($i = 0; $i < count($keys); $i++){
-					$arthropodAbsoluteDensity[$readableArthropods[$keys[$i]]] = round(($arthropodCounts[$keys[$i]] / $surveyCount) * 1, 2);
+					$arthropodAbsoluteDensity[$readableArthropods[$keys[$i]]] = round(($arthropodCounts[$keys[$i]] / $surveyCount), 2);
 				}
  				//SAVE
 				if($HIGH_TRAFFIC_MODE){
@@ -90,15 +91,39 @@
 				}
  				$arthropodPercents[strval($siteName)] = $arthropodAbsoluteDensity;
  			}
+			else if($comparisonMetric == "meanBiomass"){
+				//total biomass at site
+				$arthropodBiomasses = array();
+				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, ArthropodSighting.Length, SUM(ArthropodSighting.Quantity) AS TotalQuantity FROM `ArthropodSighting` JOIN Survey ON ArthropodSighting.SurveyFK = Survey.ID JOIN Plant ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK = '$siteID' GROUP BY ArthropodSighting.Group, ArthropodSighting.Length");
+				while($row = mysqli_fetch_assoc($query)){
+					if(!array_key_exists($row["Group"], $arthropodBiomasses)){
+						$arthropodBiomasses[$row["Group"]] = 0;
+					}
+					$arthropodBiomasses[$row["Group"]] += (getBiomass($row["Group"], $row["Length"]) * floatval($row["TotalQuantity"]));
+				}
+				//surveys at site
+				$query = mysqli_query($dbconn, "SELECT COUNT(*) AS TotalSurveyCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'");
+				$totalSurveyCount = floatval(mysqli_fetch_assoc($query)["TotalSurveyCount"]);
+				$arthropodMeanBiomass = array();
+				$keys = array_keys($arthropodSurveys);
+				for($i = 0; $i < count($keys); $i++){
+					$arthropodMeanBiomass[$readableArthropods[$keys[$i]]] = round(($arthropodBiomasses[$keys[$i]] / $totalSurveyCount), 2);
+				}
+				//SAVE
+				if($HIGH_TRAFFIC_MODE){
+					save($baseFileName, json_encode($arthropodMeanBiomass));
+				}
+ 				$arthropodPercents[strval($siteName)] = $arthropodMeanBiomass;
+			}
  			else{//relativeProportion
 				//sum of each arthropod at site
 				$arthropodCounts = array();
-				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'" . $extraSQL . " GROUP BY ArthropodSighting.Group");
+				$query = mysqli_query($dbconn, "SELECT ArthropodSighting.Group, SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' GROUP BY ArthropodSighting.Group");
 				while($row = mysqli_fetch_assoc($query)){
 					$arthropodCounts[$row["Group"]] = floatval($row["ArthropodCount"]);
 				}
 				//total survey count at site
-				$query = mysqli_query($dbconn, "SELECT SUM(ArthropodSighting.Quantity) AS AllArthropodsCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'" . $extraSQL);
+				$query = mysqli_query($dbconn, "SELECT SUM(ArthropodSighting.Quantity) AS AllArthropodsCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID'");
 				$allArthropodsCount = floatval(mysqli_fetch_assoc($query)["AllArthropodsCount"]);
 				$arthropodRelativeProportion = array();
 				$keys = array_keys($arthropodCounts);
@@ -198,6 +223,46 @@
 			}
  			die($result);
  		}
+		else if($comparisonMetric == "meanBiomass"){
+			$meanBiomassesSet = array();
+ 			$biomasses = array();
+ 			$query = mysqli_query($dbconn, "SELECT DISTINCT YEAR(LocalDate) AS Year FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE SiteFK='$siteID'");
+ 			while($row = mysqli_fetch_assoc($query)){
+				$biomasses[strval($row["Year"])] = array();
+				$meanBiomassesSet[strval($row["Year"])] = array();
+ 			}
+ 			$query = mysqli_query($dbconn, "SELECT YEAR(Survey.LocalDate) AS Year, ArthropodSighting.Group, ArthropodSighting.Length, SUM(ArthropodSighting.Quantity) AS TotalQuantity FROM `ArthropodSighting` JOIN Survey ON ArthropodSighting.SurveyFK = Survey.ID JOIN Plant ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK = '$siteID' GROUP BY YEAR(Survey.LocalDate), ArthropodSighting.Group, ArthropodSighting.Length");
+ 			while($row = mysqli_fetch_assoc($query)){
+				if(!array_key_exists($row["Group"], $biomasses[strval($row["Year"])])){
+					$biomasses[strval($row["Year"])][$row["Group"]] = 0;
+				}
+				$biomasses[strval($row["Year"])][$row["Group"]] += (getBiomass($row["Group"], $row["Length"]) * floatval($row["TotalQuantity"]));
+ 			}
+ 
+ 			$surveyCounts = array();
+ 			$query = mysqli_query($dbconn, "SELECT YEAR(Survey.LocalDate) AS Year, COUNT(Survey.ID) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' GROUP BY YEAR(Survey.LocalDate)");
+ 			while($row = mysqli_fetch_assoc($query)){
+ 				$surveyCounts[strval($row["Year"])] = $row["SurveyCount"];
+ 			}
+ 
+ 			$yearKeys = array_keys($biomasses);
+ 			foreach($yearKeys as $year) {
+				$meanBiomasses = array();
+				$arthropodKeys = array_keys($biomasses[$year]);
+				foreach($arthropodKeys as $arthropod){
+					$meanBiomasses[$readableArthropods[$arthropod]] = round(($biomasses[$year][$arthropod] / $surveyCounts[$year]), 2);
+				}
+				$meanBiomassesSet[$year] = $meanBiomasses;
+			}
+ 			uksort($meanBiomassesSet, function($a, $b){
+				return intval($a) - intval($b);
+			});
+			$result = "true|" . json_encode($meanBiomassesSet);
+			if($HIGH_TRAFFIC_MODE){
+				save($baseFileName, $result);
+			}
+ 			die($result);
+		}
  		else{//relative proportion
 			$arthropodRelativeProportionsSet = array();
  			$arthropodCounts = array();
@@ -338,6 +403,49 @@
 			}
  			die($result);
  		}
+		else if($comparisonMetric == "meanBiomass"){
+			$meanBiomassesSet = array();
+ 			$biomasses = array();
+ 			$branchCount = array();
+ 			$query = mysqli_query($dbconn, "SELECT Species, COUNT(*) AS Branches FROM Plant WHERE SiteFK='$siteID' GROUP BY Species");
+ 			while($row = mysqli_fetch_assoc($query)){
+				$biomasses[$row["Species"]] = array();
+				$meanBiomassesSet[$row["Species"] . " (" . $row["Branches"] . ")"] = array();
+				$branchCount[$row["Species"]] = $row["Branches"];
+ 			}
+ 			$query = mysqli_query($dbconn, "SELECT Plant.Species, ArthropodSighting.Group, ArthropodSighting.Length, SUM(ArthropodSighting.Quantity) AS TotalQuantity FROM `ArthropodSighting` JOIN Survey ON ArthropodSighting.SurveyFK = Survey.ID JOIN Plant ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK = '$siteID' GROUP BY Plant.Species, ArthropodSighting.Group, ArthropodSighting.Length");
+ 			while($row = mysqli_fetch_assoc($query)){
+				if(!array_key_exists($row["Group"], $biomasses[$row["Species"]])){
+					$biomasses[$row["Species"]][$row["Group"]] = 0;
+				}
+				$biomasses[$row["Species"]][$row["Group"]] += (getBiomass($row["Group"], $row["Length"]) * floatval($row["TotalQuantity"]));
+ 			}
+ 
+ 			$surveyCounts = array();
+ 			$query = mysqli_query($dbconn, "SELECT Plant.Species, COUNT(Survey.ID) AS SurveyCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' GROUP BY Plant.Species");
+ 			while($row = mysqli_fetch_assoc($query)){
+ 				$surveyCounts[$row["Species"]] = $row["SurveyCount"];
+ 			}
+ 			
+			$speciesKeys = array_keys($biomasses);
+ 			foreach($speciesKeys as $species){
+				$meanBiomasses = array();
+				$arthropodKeys = array_keys($biomasses[$species]);
+				foreach($arthropodKeys as $arthropod){
+					$meanBiomasses[$readableArthropods[$arthropod]] = round(($biomasses[$species][$arthropod] / $surveyCounts[$species]), 2);
+				}
+				$meanBiomassesSet[$species . " (" . $branchCount[$species] . ")"] = $meanBiomasses;
+			}
+ 			uksort($meanBiomassesSet, function($a, $b){
+				global $order;
+				return array_search(substr($b, 0, strrpos($b, " (")), $order) - array_search(substr($a, 0, strrpos($a, " (")), $order);
+			});
+			$result = "true|" . json_encode($meanBiomassesSet);
+			if($HIGH_TRAFFIC_MODE){
+				save($baseFileName, $result);
+			}
+ 			die($result);
+		}
  		else{//relative proportion
 			$arthropodRelativeProportionsSet = array();
  			$arthropodCounts = array();
