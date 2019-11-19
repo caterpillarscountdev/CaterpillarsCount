a65a45d20956ab4a4b8133feda4d1b21fca1dd46
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2019-11-18 19:23:09 -0500

    adjust resultMemory path

--- php/getCompositionWithBiomass.php
+++ php/getCompositionWithBiomass.php
@@ -1,6 +1,6 @@
 <?php
  	require_once('orm/resources/Keychain.php');
-	require_once('tools/resultMemory.php');
+	require_once('resultMemory.php');
 	require_once('tools/biomassCalculator.php');
    
  	$siteIDs = json_decode($_GET["siteIDs"]);
