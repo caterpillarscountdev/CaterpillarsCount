f9652225bb7b7efbae47e5baa78455365407c546
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-02-17 00:45:21 -0500

    leading group and original group

--- dataDownload/index.php
+++ dataDownload/index.php
@@ -19,7 +19,8 @@
 		"NumberOfLeaves", 
 		"AverageLeafLength", 
 		"HerbivoryScore", 
-		"ArthropodGroup", 
+		"OriginalArthropodGroup", 
+		"LeadingArthropodGroup", 
 		"ArthropodLength", 
 		"ArthropodQuantity", 
 		"ArthropodPhotoURL", 
@@ -41,7 +42,7 @@
 		$tableArray = array();
 
 		$dbconn = (new Keychain)->getDatabaseConnection();
-		$query = mysqli_query($dbconn, "SELECT Survey.ID, Survey.LocalDate, SUBSTR(Survey.LocalTime, 1, 5) AS `LocalTime`, Plant.Code AS SurveyLocationCode, Plant.Circle, Plant.Orientation, Survey.PlantSpecies AS PlantSpeciesMarkedByObserver, Plant.Species AS OfficialPlantSpecies, Site.Name AS SiteName, Site.Description AS SiteDescription, Site.Latitude, Site.Longitude, Site.Region, ArthropodSighting.LeadingGroup AS ArthropodGroup, ArthropodSighting.Length AS ArthropodLength, ArthropodSighting.Quantity AS ArthropodQuantity, IF(ArthropodSighting.PhotoURL='','',CONCAT('https://caterpillarscount.unc.edu/images/arthropods/', ArthropodSighting.PhotoURL)) AS ArthropodPhotoURL, ArthropodSighting.Notes AS ArthropodNotes, ArthropodSighting.Hairy AS IsCaterpillarAndIsHairy, ArthropodSighting.Rolled AS IsCaterpillarAndIsInLeafRoll, ArthropodSighting.Tented AS IsCaterpillarAndIsInSilkTent, Survey.ObservationMethod, Survey.Notes AS SurveyNotes, Survey.WetLeaves, Survey.NumberOfLeaves, Survey.AverageLeafLength, Survey.HerbivoryScore FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Site.ID<>'2' AND Plant.SiteFK LIKE '$siteID' AND YEAR(Survey.LocalDate)>='$yearStart' AND YEAR(Survey.LocalDate)<='$yearEnd' AND ArthropodSighting.LeadingGroup LIKE '$arthropod' ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC");
+		$query = mysqli_query($dbconn, "SELECT Survey.ID, Survey.LocalDate, SUBSTR(Survey.LocalTime, 1, 5) AS `LocalTime`, Plant.Code AS SurveyLocationCode, Plant.Circle, Plant.Orientation, Survey.PlantSpecies AS PlantSpeciesMarkedByObserver, Plant.Species AS OfficialPlantSpecies, Site.Name AS SiteName, Site.Description AS SiteDescription, Site.Latitude, Site.Longitude, Site.Region, ArthropodSighting.OriginalGroup AS OriginalArthropodGroup, ArthropodSighting.LeadingGroup AS LeadingArthropodGroup, ArthropodSighting.Length AS ArthropodLength, ArthropodSighting.Quantity AS ArthropodQuantity, IF(ArthropodSighting.PhotoURL='','',CONCAT('https://caterpillarscount.unc.edu/images/arthropods/', ArthropodSighting.PhotoURL)) AS ArthropodPhotoURL, ArthropodSighting.Notes AS ArthropodNotes, ArthropodSighting.Hairy AS IsCaterpillarAndIsHairy, ArthropodSighting.Rolled AS IsCaterpillarAndIsInLeafRoll, ArthropodSighting.Tented AS IsCaterpillarAndIsInSilkTent, Survey.ObservationMethod, Survey.Notes AS SurveyNotes, Survey.WetLeaves, Survey.NumberOfLeaves, Survey.AverageLeafLength, Survey.HerbivoryScore FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Site.ID<>'2' AND Plant.SiteFK LIKE '$siteID' AND YEAR(Survey.LocalDate)>='$yearStart' AND YEAR(Survey.LocalDate)<='$yearEnd' AND ArthropodSighting.LeadingGroup LIKE '$arthropod' ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC");
 		
 		//ROWS
 		$surveyIDsWithSightings = array();
@@ -62,7 +63,7 @@
 				if(array_key_exists($colHeaders[$i], $row)){
 					$rowArray[] = $row[$colHeaders[$i]];
 				}
-				else if($colHeaders[$i] == "ArthropodGroup"){
+				else if($colHeaders[$i] == "OriginalArthropodGroup" || $colHeaders[$i] == "LeadingArthropodGroup"){
 					$rowArray[] = "none";
 				}
 				else{
