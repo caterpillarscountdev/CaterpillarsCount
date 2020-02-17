f0a9ddef9ea412c985e7108eab199cf4b1313fdb
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-02-17 00:19:38 -0500

    $arthropodData[$i] to $arthropodData[$j]

--- php/editSurvey.php
+++ php/editSurvey.php
@@ -53,9 +53,9 @@
 				for($i = 0; $i < count($arthropodSightings); $i++){
 					for($j = 0; $j < count($arthropodData); $j++){
 						if(strval($arthropodSightings[$i]->getID()) == strval($arthropodData[$j][0])){
-							$updateResult = $arthropodSightings[$i]->setAllEditables($arthropodData[$i][1], $arthropodData[$i][2], $arthropodData[$i][3], $arthropodData[$i][4], $arthropodData[$i][5], $arthropodData[$i][6], $arthropodData[$i][7], $arthropodData[$i][8], $arthropodData[$i][9]);
+							$updateResult = $arthropodSightings[$i]->setAllEditables($arthropodData[$j][1], $arthropodData[$j][2], $arthropodData[$j][3], $arthropodData[$j][4], $arthropodData[$j][5], $arthropodData[$j][6], $arthropodData[$j][7], $arthropodData[$j][8], $arthropodData[$j][9]);
 							if($updateResult === false){
-								$failures .= "Could not locate " . $arthropodData[$i][1] . " sighting record. ";
+								$failures .= "Could not locate " . $arthropodData[$j][1] . " sighting record. ";
 							}
 							else if($updateResult !== true){
 								$failures .= $updateResult;
