9698e3d1742ba054fade7700d984b6b84820eacb
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-02-27 21:12:42 -0500

    escape htmlentities

--- php/trackDownload.php
+++ php/trackDownload.php
@@ -6,9 +6,9 @@
   $date = date("Y-m-d");
   $time = date("H:i:s");
   $ip = $_SERVER['REMOTE_ADDR'];
-  $page = mysqli_real_escape_string($dbconn, trim(rawurldecode($_GET["page"])));
-  $file = mysqli_real_escape_string($dbconn, trim(rawurldecode($_GET["file"])));
-  $filters = mysqli_real_escape_string($dbconn, trim(rawurldecode($_GET["filters"])));
+  $page = mysqli_real_escape_string($dbconn, trim(htmlentities(rawurldecode($_GET["page"]))));
+  $file = mysqli_real_escape_string($dbconn, trim(htmlentities(rawurldecode($_GET["file"]))));
+  $filters = mysqli_real_escape_string($dbconn, trim(htmlentities(rawurldecode($_GET["filters"]))));
   
   mysqli_query($dbconn, "INSERT INTO Download (`Date`, `UTCTime`, `IP`, `Page`, `File`, `Filters`) VALUES ('$date', '$time', '$ip', '$page', '$file', '$filters')");
   mysqli_close($dbconn);
