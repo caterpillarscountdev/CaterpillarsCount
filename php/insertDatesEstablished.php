ed9153069dd4b90fadf9ed422b6bcad102572dfb
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-04-11 20:30:44 -0400

    Update insertDatesEstablished.php

--- php/insertDatesEstablished.php
+++ php/insertDatesEstablished.php
@@ -7,7 +7,7 @@
   while($row = mysqli_fetch_assoc($query)){
     $innerQuery = mysqli_query($dbconn, "SELECT LocalDate AS EarliestDate FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='" . intval($row["ID"]) . "' ORDER BY LocalDate ASC LIMIT 1");
     if(mysqli_num_rows($innerQuery) == 1){
-      mysqli_query($dbconn, "UPDATE Site SET `DateEstablished`='" . $innerQuery["EarliestDate"] . "'");
+      mysqli_query($dbconn, "UPDATE Site SET `DateEstablished`='" . mysqli_fetch_assoc($innerQuery)["EarliestDate"] . "'");
     }
   }
 ?>
