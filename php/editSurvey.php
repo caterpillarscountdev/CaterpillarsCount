402ab43740b7e532452b0d7791f29d5d4af24e39
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-05-03 11:37:23 -0400

    upload new photos if included and mark them for iNaturalist submission

--- php/editSurvey.php
+++ php/editSurvey.php
@@ -18,6 +18,47 @@
 	$numberOfLeaves = $_POST["numberOfLeaves"];		//number
 	$averageLeafLength = $_POST["averageLeafLength"];	//number
 	$herbivoryScore = $_POST["herbivoryScore"];		//String
+
+	function explainError($fileError){
+		if($fileError == UPLOAD_ERR_INI_SIZE){return 'The uploaded file exceeds the upload_max_filesize directive in php.ini. ';}
+		if($fileError == UPLOAD_ERR_FORM_SIZE){return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form. ';}
+		if($fileError == UPLOAD_ERR_PARTIAL){return 'The uploaded file was only partially uploaded. ';}
+		if($fileError == UPLOAD_ERR_NO_FILE){return 'No file was uploaded. ';}
+		if($fileError == UPLOAD_ERR_NO_TMP_DIR){return 'Missing a temporary folder. Introduced in PHP 5.0.3. ';}
+		if($fileError == UPLOAD_ERR_CANT_WRITE){return 'Failed to write file to disk. Introduced in PHP 5.1.0. ';}
+		if($fileError == UPLOAD_ERR_EXTENSION){return 'A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help. Introduced in PHP 5.2.0. ';}
+		return 'Upload unsuccessful. ';
+	}
+		
+	function attachPhotoToArthropodSighting($file, $arthropodSighting){
+		if(!is_uploaded_file($file['tmp_name'])){
+			return "File not uploaded. ";
+		}
+		
+		$fileName = $file['name'];
+		$fileType = $file['type'];
+		$fileType = str_replace("image/", "", strToLower($fileType));
+		$fileError = $file['error'];
+		$fileContent = file_get_contents($file['tmp_name']);
+		$path = "../images/arthropods/";
+		$name = $arthropodSighting->getID() . "." . $fileType;
+			
+		if($fileError == UPLOAD_ERR_OK){
+			//Processes your file here
+			if(file_exists($path . $name) && !unlink($path . $name)){
+				return "Could not overwrite arthropod photo. ";
+			}
+				
+			if(in_array($fileType, array("png", "jpg", "jpeg", "gif"))){
+				if(move_uploaded_file($file["tmp_name"], $path . $name)){
+					return $arthropodSighting->setPhotoURL($name, true);
+				}
+				return "Unable to transfer file to server. ";
+			}
+			return "file type must be an image. ";
+		}
+		return explainError($fileError);
+	}
 	
   	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
@@ -59,6 +100,14 @@
 							}
 							else if($updateResult !== true){
 								$failures .= $updateResult;
+							}
+							else{
+								//if we successfully set all editables
+								//add a photo to the arthropod sighting if it exists
+								$attachResult = attachPhotoToArthropodSighting($_FILES['file' . $i], $arthropodSightings[$i]);
+								if($attachResult != "File not uploaded. " && $attachResult !== true){
+									$failures .= "Photo #" . $j . ": " . strval($attachResult);
+								}
 							}
 						}
 					}
