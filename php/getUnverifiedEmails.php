f7fec74d6900192e6caac6eaa6f85f9cc6d3ddf0
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-05-10 16:23:04 -0400

    if not user and unvalidated

--- php/getUnverifiedEmails.php
+++ php/getUnverifiedEmails.php
@@ -6,7 +6,7 @@
   $actuallyUnverifiedEmails = array();
   for($i = 0; $i < count($unverifiedEmails); $i++){
     $user = User::findByEmail($unverifiedEmails[$i]);
-    if(!(is_object($user) && get_class($user) == "User") && !(User::emailIsUnvalidated($unverifiedEmails[$i]))){
+    if(!(is_object($user) && get_class($user) == "User") && User::emailIsUnvalidated($unverifiedEmails[$i])){
       $actuallyUnverifiedEmails[] = filter_var(rawurldecode($unverifiedEmails[$i]), FILTER_SANITIZE_EMAIL);
     }
   }
