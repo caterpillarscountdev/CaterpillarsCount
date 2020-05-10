002d50aecd331c0a53155077684e861156310701
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2020-05-10 15:16:41 -0400

    Create getUnverifiedEmails.php

---
+++ php/getUnverifiedEmails.php
@@ -0,0 +1,12 @@
+<?php
+  $unverifiedEmails = json_decode($_GET["unverifiedEmails"]);
+  
+  $actuallyUnverifiedEmails = array();
+  for($i = 0; $i < count($unverifiedEmails); $i++){
+    $user = User::findByEmail($unverifiedEmails[$i]);
+    if(!is_object($user) || get_class($user) !== "User" || !(User::emailIsUnvalidated($unverifiedEmails[$i]))){
+      $actuallyUnverifiedEmails[] = filter_var(rawurldecode($unverifiedEmails[$i]), FILTER_SANITIZE_EMAIL);
+    }
+  }
+  die(json_encode($actuallyUnverifiedEmails));
+?>
