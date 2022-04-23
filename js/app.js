690e924cafaf962c1a666b13ec4a61a31edeff53
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-04-23 17:39:02 -0400

    temporarily disable photo uploads

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+  //temporarily disable photo uploads
+  $("#arthropodPhotoGroup").css({opacity:"0.5"});
+  var arthropodFileInputHolderElement = $("#arthropodFileInputHolder");
+  if(arthropodFileInputHolderElement.length > 0){
+    arthropodFileInputHolderElement[0].onclick = function(){
+      queueNotice("error", "Photo uploads are undergoing maintenance. Please submit this survey without a photo.");
+    };
+  }
+});
