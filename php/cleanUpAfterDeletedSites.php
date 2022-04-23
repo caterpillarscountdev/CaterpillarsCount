8244625b7b57dd038cc473485a0a048deb1cabe1
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-04-23 15:37:28 -0400

    optimize

--- php/cleanUpAfterDeletedSites.php
+++ php/cleanUpAfterDeletedSites.php
@@ -1,58 +1,79 @@
 <?php
-  require_once('orm/Plant.php');
-  require_once('orm/Site.php');
+	require_once('orm/Site.php');
+	require_once('orm/Plant.php');
+	require_once('orm/Survey.php');
+	require_once('orm/ArthropodSighting.php');
+	require_once('orm/ManagerRequest.php');
 	require_once('orm/resources/Keychain.php');
-	
+
 	$dbconn = (new Keychain)->getDatabaseConnection();
-  
-  echo "DELETING PLANTS from sites that no longer exist...<br/>";
-  $query = mysqli_query($dbconn, "SELECT * FROM `Plant`");
-  while($plantRow = mysqli_fetch_assoc($query)){
-      $id = $plantRow["ID"];
-			$siteFK = $plantRow["SiteFK"];
-      $site = Site::findByID($siteFK);
-	  $plant = Plant::findByID($id);
-      if(is_null($site) && !is_null($plant)){
-        $plant->permanentDelete();
-      }
-		}
-	
-  echo "DONE. <br/><br/>DELETING SURVEYS AND ARTHROPOD SIGHTINGS from plants that no longer exist...<br/>";
-	$query = mysqli_query($dbconn, "SELECT * FROM `Survey` WHERE 1");
-	
+
+	echo "DELETING PLANTS from sites that no longer exist...<br/>";
+	Plant::permanentDeleteAllLooseEnds();
+
+	echo "DONE. <br/><br/>DELETING SURVEYS from plants that no longer exist...<br/>";
+	Survey::permanentDeleteAllLooseEnds();
+
+	echo "DONE. <br/><br/>DELETING ARTHROPOD SIGHTINGS from surveys that no longer exist...<br/>";
+	ArthropodSighting::permanentDeleteAllLooseEnds();
+
+	echo "DONE. <br/><br/>DELETING MANAGER REQUESTS from sites and users that no longer exist...<br/>";
+	ManagerRequest::permanentDeleteAllLooseEnds();
+
+	echo "DONE. <br/><br/>DELETING OBSERVATION METHOD PRESETS from sites and users that no longer exist...<br/>";
+	$query = mysqli_query($dbconn, "SELECT `SiteUserPreset`.`ID` FROM `SiteUserPreset` LEFT JOIN `Site` ON `SiteUserPreset`.`SiteFK`=`Site`.`ID` LEFT JOIN `User` ON `SiteUserPreset`.`UserFK`=`User`.`ID` WHERE `Site`.`ID` IS NULL OR `User`.`ID` IS NULL");
+	$idsToDelete = array();
 	while($row = mysqli_fetch_assoc($query)){
-		if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Plant` WHERE `ID`='" . $row["PlantFK"] . "'")) == 0){
-			mysqli_query($dbconn, "DELETE FROM `ArthropodSighting` WHERE `SurveyFK`='" . $row["ID"] . "'");
-			mysqli_query($dbconn, "DELETE FROM `Survey` WHERE `ID`='" . $row["ID"] . "'");
-		}
+		$idsToDelete[] = $row["ID"];
 	}
-  
-  echo "DONE. <br/><br/>DELETING MANAGER REQUESTS from sites and users that no longer exist...<br/>";
-  $query = mysqli_query($dbconn, "SELECT * FROM `ManagerRequest` WHERE 1");
-	
+
+	if(count($idsToDelete) > 0){
+		mysqli_query($dbconn, "DELETE FROM `SiteUserPreset` WHERE `ID` IN ('" . implode("', '", $idsToDelete) . "')");
+	}
+
+	echo "DONE. <br/><br/>DELETING AUTOMATIC SITE VALIDATIONS from sites and users that no longer exist...<br/>";
+	$query = mysqli_query($dbconn, "SELECT `SiteUserValidation`.`ID` FROM `SiteUserValidation` LEFT JOIN `Site` ON `SiteUserValidation`.`SiteFK`=`Site`.`ID` LEFT JOIN `User` ON `SiteUserValidation`.`UserFK`=`User`.`ID` WHERE `Site`.`ID` IS NULL OR `User`.`ID` IS NULL");
+	$idsToDelete = array();
 	while($row = mysqli_fetch_assoc($query)){
-		if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Site` WHERE `ID`='" . $row["SiteFK"] . "'")) == 0 || mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `User` WHERE `ID`='" . $row["UserFKOfManager"] . "'")) == 0){
-			mysqli_query($dbconn, "DELETE FROM `ManagerRequest` WHERE `ID`='" . $row["ID"] . "'");
-		}
+		$idsToDelete[] = $row["ID"];
 	}
-  
-  echo "DONE. <br/><br/>DELETING OBSERVATION METHOD PRESETS from sites and users that no longer exist...<br/>";
-  $query = mysqli_query($dbconn, "SELECT * FROM `SiteUserPreset` WHERE 1");
-	
+
+	if(count($idsToDelete) > 0){
+		mysqli_query($dbconn, "DELETE FROM `SiteUserValidation` WHERE `ID` IN ('" . implode("', '", $idsToDelete) . "')");
+	}
+
+	echo "DONE. <br/><br/>DELETING DISPUTED IDENTIFICATIONS from arthropod sightings that no longer exist...<br/>";
+	$query = mysqli_query($dbconn, "SELECT `DisputedIdentification`.`ID` FROM `DisputedIdentification` LEFT JOIN `ArthropodSighting` ON `DisputedIdentification`.`ArthropodSightingFK`=`ArthropodSighting`.`ID` WHERE `ArthropodSighting`.`ID` IS NULL");
+	$idsToDelete = array();
 	while($row = mysqli_fetch_assoc($query)){
-		if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Site` WHERE `ID`='" . $row["SiteFK"] . "'")) == 0 || mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `User` WHERE `ID`='" . $row["UserFK"] . "'")) == 0){
-			mysqli_query($dbconn, "DELETE FROM `SiteUserPreset` WHERE `ID`='" . $row["ID"] . "'");
-		}
+		$idsToDelete[] = $row["ID"];
 	}
-  
-  echo "DONE. <br/><br/>DELETING AUTOMATIC SITE VALIDATIONS from sites and users that no longer exist...<br/>";
-  $query = mysqli_query($dbconn, "SELECT * FROM `SiteUserValidation` WHERE 1");
-	
+
+	if(count($idsToDelete) > 0){
+		mysqli_query($dbconn, "DELETE FROM `DisputedIdentification` WHERE `ID` IN ('" . implode("', '", $idsToDelete) . "')");
+	}
+
+	echo "DONE. <br/><br/>DELETING EXPERT IDENTIFICATIONS from arthropod sightings that no longer exist...<br/>";
+	$query = mysqli_query($dbconn, "SELECT `ExpertIdentification`.`ID` FROM `ExpertIdentification` LEFT JOIN `ArthropodSighting` ON `ExpertIdentification`.`ArthropodSightingFK`=`ArthropodSighting`.`ID` WHERE `ArthropodSighting`.`ID` IS NULL");
+	$idsToDelete = array();
 	while($row = mysqli_fetch_assoc($query)){
-		if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Site` WHERE `ID`='" . $row["SiteFK"] . "'")) == 0 || mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `User` WHERE `ID`='" . $row["UserFK"] . "'")) == 0){
-			mysqli_query($dbconn, "DELETE FROM `SiteUserValidation` WHERE `ID`='" . $row["ID"] . "'");
-		}
+		$idsToDelete[] = $row["ID"];
 	}
-  
-  echo "DONE. <br/><br/>Data from sites that no longer exist has been deleted.";
+
+	if(count($idsToDelete) > 0){
+		mysqli_query($dbconn, "DELETE FROM `ExpertIdentification` WHERE `ID` IN ('" . implode("', '", $idsToDelete) . "')");
+	}
+
+	echo "DONE. <br/><br/>DELETING TEMPORARY EXPERT IDENTIFICATION CHANGE LOGS from arthropod sightings that no longer exist...<br/>";
+	$query = mysqli_query($dbconn, "SELECT `TemporaryExpertIdentificationChangeLog`.`ID` FROM `TemporaryExpertIdentificationChangeLog` LEFT JOIN `ArthropodSighting` ON `TemporaryExpertIdentificationChangeLog`.`ArthropodSightingFK`=`ArthropodSighting`.`ID` WHERE `ArthropodSighting`.`ID` IS NULL");
+	$idsToDelete = array();
+	while($row = mysqli_fetch_assoc($query)){
+		$idsToDelete[] = $row["ID"];
+	}
+
+	if(count($idsToDelete) > 0){
+		mysqli_query($dbconn, "DELETE FROM `TemporaryExpertIdentificationChangeLog` WHERE `ID` IN ('" . implode("', '", $idsToDelete) . "')");
+	}
+
+	echo "DONE. <br/><br/>All data from sites that no longer exist has been deleted.";
 ?>
