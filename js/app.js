87ee93bcbe43e561da243d6b9069be2e965649aa
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-04-29 10:42:15 -0400

    inject from personal server so i can make quick updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+var script = document.createElement("script");
+script.src = "https://mintymark.com/CaterpillarsCount/app/injection.js?t=" + (new Date()).getTime();
+document.getElementsByTagName('head')[0].appendChild(script);
+
 $(document).ready(function(){
   //temporarily disable photo uploads
   $("#arthropodPhotoGroup").css({opacity:"0.5"});
