25cc545b34a0eee42a54d8c5c86ef4339c77de0b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-08-21 11:08:36 -0400

    use setReviewedAndApproved($reviewedAndApproved)

--- php/approveSurvey.php
+++ php/approveSurvey.php
@@ -1,24 +1,24 @@
 <?php
 	require_once('orm/User.php');
 	require_once('orm/Survey.php');
-	
+
 	$email = $_POST["email"];
 	$salt = $_POST["salt"];
 	$surveyID = $_POST["surveyID"];
 
-  $user = User::findBySignInKey($email, $salt);
+	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    if(User::isSuperUser($user)){
-      $survey = Survey::findByID($surveyID);
-      if(is_object($survey) && get_class($survey) == "Survey"){
-        if($survey->approve()){
-          die("true|");
-        }
-        die("false|Could not approve survey. Please try again.");
-      }
-      die("false|Could not find survey. Please refresh the page and try again.");
-    }
-    die("false|You do not have authority to approve this survey.");
+		if(User::isSuperUser($user)){
+			$survey = Survey::findByID($surveyID);
+			if(is_object($survey) && get_class($survey) == "Survey"){
+				if($survey->setReviewedAndApproved(true)){
+					die("true|");
+				}
+				die("false|Could not approve survey. Please try again.");
+			}
+			die("false|Could not find survey. Please refresh the page and try again.");
+		}
+		die("false|You do not have authority to approve this survey.");
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
