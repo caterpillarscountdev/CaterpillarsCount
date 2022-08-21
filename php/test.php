fde9d90bd0fd24f3e3df99958443083189947d93
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-08-20 23:05:26 -0400

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,5 +1,12 @@
 <?php
+  require_once('orm/User.php');
   require_once "orm/Survey.php";
+
+  $email = $_GET["email"];
+	$salt = $_GET["salt"];
   
-  die(Survey::getTest());
+  $user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    die(Survey::getTest($user));
+  }
 ?>
