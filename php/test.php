16341c670d9b079ceb2f29cf685acc703d2a13ad
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2022-08-20 22:57:32 -0400

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,53 +1,5 @@
 <?php
-	require_once("orm/User.php");
-	require_once("orm/Site.php");
-	require_once("orm/Plant.php");
-
-// 	$email = $_GET["email"];
-// 	$salt = $_GET["salt"];
-	
-// 	$user = User::findBySignInKey($email, $salt);
-// 	if(is_object($user) && get_class($user) == "User"){
-//     		$site = Site::findByID(255);
-// 		if(is_object($site) && get_class($site) == "Site"){
-//       			die($site->isAuthority($user) ? "IS_AUTHORITY" : "NOT_AUTHORITY");
-//     		}
-//     		die("NOT A SITE");
-//   	}
-// 	die("NOT A USER");
-
-
-	$users = User::findUsersByIDs(array(2066, 25));
-	$userCount = 0;
-	for($i = 0; $i < count($users); $i++){
-		try{
-			$userID = $users[$i]->getID();
-			if($userID == 2066 || $userID == 25){
-				$userCount++;
-			}
-		}catch(Exception $e){}
-	}
-
-	$plants = Plant::findPlantsByIDs(array(5663, 5664));
-	$plantCount = 0;
-	for($i = 0; $i < count($plants); $i++){
-		try{
-			$plantID = $plants[$i]->getID();
-			if($plantID == 5663 || $plantID == 5664){
-				$plantCount++;
-			}
-		}catch(Exception $e){}
-	}
-
-	$siteID = $plants[0]->getSite()->getID();
-
-	echo "[" . Site::findByID(255)->getCreator()->getID() . "]";
-
-	try{
-		$creatorID = $plants[0]->getSite()->getCreator()->getID();
-		echo "success";
-	}
-	catch(Exception $e){echo "failed";}
-
-	//echo $userCount . "|" . $plantCount . "|" . $siteID . "|" . $creatorID;
+  require_once "orm/Survey.php";
+  
+  die(Survey::getTest());
 ?>
