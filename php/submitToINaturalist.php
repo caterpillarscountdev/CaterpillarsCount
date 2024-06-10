64e00a30bb99009e78318108cbdb2de1ce635730
Luke Opperman <luke@opperman.net>
2024-06-10 07:12:34 -0400

    Send survey time with date to iNaturalist

--- php/submitToINaturalist.php
+++ php/submitToINaturalist.php
@@ -18,7 +18,7 @@
 		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 	}
 
-	function submitINaturalistObservation($dbconn, $arthropodSightingID, $userTag, $plantCode, $date, $observationMethod, $surveyNotes, $wetLeaves, $order, $hairy, $rolled, $tented, $sawfly, $beetleLarva, $arthropodQuantity, $arthropodLength, $arthropodPhotoURL, $arthropodNotes, $numberOfLeaves, $averageLeafLength, $herbivoryScore){
+        function submitINaturalistObservation($dbconn, $arthropodSightingID, $userTag, $plantCode, $date, $time, $observationMethod, $surveyNotes, $wetLeaves, $order, $hairy, $rolled, $tented, $sawfly, $beetleLarva, $arthropodQuantity, $arthropodLength, $arthropodPhotoURL, $arthropodNotes, $numberOfLeaves, $averageLeafLength, $herbivoryScore){
 		//GET AUTHORIZATION
 		$debuginat = false; // turn all debugging comments off with this variable
 		if ($debuginat==true) {  echo("<!-- submitINaturalistObservation .. init for ID " . $arthropodSightingID . " -->");}
@@ -79,7 +79,7 @@
 			"observation" => array(
 				"species_guess" => cleanParameter($newOrder),
 				"id_please" => 1,
-				"observed_on_string" => cleanParameter($date),
+				"observed_on_string" => cleanParameter($date . ' ' . $time),
 				"place_guess" => cleanParameter($site->getName()),
 				"latitude" => cleanParameter($site->getLatitude()),
 				"longitude" => cleanParameter($site->getLongitude())
