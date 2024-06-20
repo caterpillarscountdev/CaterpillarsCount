9cdd899f33b96fdf1bb5f0f38153f66af1481e0f
Luke Opperman <luke@opperman.net>
2024-06-20 11:31:04 -0400

    Revise duplicate survey monitor to only last week, find possible duplicate IDS, format

--- php/monitorSurveys.php
+++ php/monitorSurveys.php
@@ -1,26 +1,31 @@
 <?php
-  	require_once('/opt/app-root/src/php/orm/resources/Keychain.php');
-  	require_once('/opt/app-root/src/php/orm/resources/mailing.php');
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	
-	$mobileTimeouts = array();
-	$query = mysqli_query($dbconn, "SELECT Survey.ID, Plant.Code, Survey.LocalDate, Survey.LocalTime, CONCAT(User.FirstName, \" \", User.LastName) AS FullName FROM Survey JOIN User ON Survey.UserFKOfObserver = User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Survey.SubmittedThroughApp='1' AND Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID > '100000' GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY Survey.SubmittedThroughApp DESC, `Survey`.`SubmissionTimestamp` ASC");
+        $duplicates = array();
+        $ids = array();
+        $others = array();
+        
+	$query = mysqli_query($dbconn, "SELECT Survey.ID, Plant.Code, Survey.LocalDate, Survey.LocalTime, CONCAT(User.FirstName, \" \", User.LastName) AS FullName FROM Survey JOIN User ON Survey.UserFKOfObserver = User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.LocalDate >= DATE_SUB(NOW(), INTERVAL 1 WEEK) GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, SubmittedThroughApp, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY Survey.SubmittedThroughApp DESC, Survey.SubmissionTimestamp ASC");
 	if(mysqli_num_rows($query) > 0){
 		while($row = mysqli_fetch_assoc($query)){
-			$mobileTimeouts[] = "SURVEY ID: " . $row["ID"] . "| " . $row["Code"] . " " . $row["LocalDate"] . " " . $row["LocalTime"] . " " . $row["FullName"];
+                  $ids[] = $row["ID"];
+                  $duplicates[] = "SURVEY ID: " . $row["ID"] . "| " . $row["Code"] . " " . $row["LocalDate"] . " " . $row["LocalTime"] . " " . $row["FullName"];
 		}
 	}
-	
-	$desktopResubmissions = array();
-	$query = mysqli_query($dbconn, "SELECT Survey.ID, Plant.Code, Survey.LocalDate, Survey.LocalTime, CONCAT(User.FirstName, \" \", User.LastName) AS FullName FROM Survey JOIN User ON Survey.UserFKOfObserver = User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Survey.SubmittedThroughApp='0' AND Plant.SiteFK<>'2' AND CORRESPONDING_OLD_DATABASE_SURVEY_ID='0' AND Survey.ID > '100000' GROUP BY UserFKOfObserver, PlantFK, LocalDate, `LocalTime`, ObservationMethod, Notes, WetLeaves, PlantSpecies, NumberOfLeaves, AverageLeafLength, HerbivoryScore, SubmittedThroughApp, MinimumTemperature, MaximumTemperature, CORRESPONDING_OLD_DATABASE_SURVEY_ID HAVING COUNT(*) > 1 ORDER BY Survey.SubmittedThroughApp DESC, `Survey`.`SubmissionTimestamp` ASC");
+	  
+        if(count($duplicates) > 0) {
+
+          $query = mysqli_query($dbconn, "SELECT S1.ID, S2.ID AS Duplicate FROM Survey AS S1 LEFT JOIN Survey AS S2 ON (S1.ID != S2.ID AND S1.PlantFK = S2.PlantFK AND S1.UserFKOfObserver = S2.UserFKOfObserver AND S1.LocalDate = S2.LocalDate AND S1.LocalTime = S2.LocalTime)  WHERE S1.ID IN (" . join(", ", $ids) . ") ORDER BY S1.SubmissionTimestamp ASC");
 	if(mysqli_num_rows($query) > 0){
 		while($row = mysqli_fetch_assoc($query)){
-			$desktopResubmissions[] = "SURVEY ID: " . $row["ID"] . "| " . $row["Code"] . " " . $row["LocalDate"] . " " . $row["LocalTime"] . " " . $row["FullName"];
+                  $others[] = "SURVEY ID: " . $row["ID"] . " possible duplicate: " . $row["Duplicate"];
 		}
 	}
-  
-  	if(count($mobileTimeouts) > 0 || count($desktopResubmissions) > 0){
-		email("caterpillarscount@office.unc.edu", "Duplicate survey detected", "There may have been a timeout or some other error that caused a user to have to submit the same survey twice. The following Survey IDs should be investigated: \n\n\nMOBILE APP:\n" . implode("\n\n", $mobileTimeouts) . "\n\n\nWEBSITE:\n" . implode("\n\n", $desktopResubmissions));
+          
+
+
+        $body = "<html><body><p>There may have been a timeout or some other error that caused a user to have to submit the same survey twice. The following Survey IDs should be investigated:</p>" . implode("<p>", $duplicates) . "<p><p>" . implode("<p>", $others);
+        email("caterpillarscount@office.unc.edu", "Duplicate survey detected", $body );
   	}
 ?>
