949db773cae0d46730adf5d3f7aa42fbd77f068f
Luke Opperman <luke@opperman.net>
2024-11-21 10:48:11 -0500

    Add columns to existing tables

--- data/sql-updates/survey-flagged.sql
+++ data/sql-updates/survey-flagged.sql
@@ -1,22 +1,30 @@
-CREATE TABLE `SurveyFlagged` (
-  `ID` int(11) NOT NULL,
-  `SurveyFK` int(11) NOT NULL,
-  `NumberOfLeavesLow` int(1),
-  `NumberOfLeavesHigh` int(1),
-  `NumberOfLeavesOverride` int(1),
-  `AverageLeafLengthHigh` int(1),
-  `AverageLeafLengthOverride` int(1),
-  `ArthropodLengthHigh` int(1),
-  `ArthropodLengthOverride` int(1),
-  `ArthropodQuantityHigh` int(1),
-  `ArthropodQuantityOverride` int(1), 
-  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `DateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `Survey`
+  ADD COLUMN `ReviewedAndApprovedSite` tinyint(1) DEFAULT 0;
 
-ALTER TABLE `SurveyFlagged`
-  ADD PRIMARY KEY (`ID`);
-ALTER TABLE `SurveyFlagged`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-ALTER TABLE `SurveyFlagged`
-  ADD CONSTRAINT unique_survey UNIQUE INDEX (SurveyFK);
+ALTER TABLE `Survey`
+  ADD COLUMN `QCNumberOfLeavesHigh` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCNumberOfLeavesOK` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCAverageLeafLengthHigh` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCAverageLeafLengthOK` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCArthropodLengthHigh` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCArthropodLengthOK` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCArthropodQuantityHigh` tinyint(1) ;
+ALTER TABLE `Survey`
+  ADD COLUMN `QCArthropodQuantityOK` tinyint(1) ;
+
+ALTER TABLE `ArthropodSighting`
+  ADD COLUMN `QCArthropodLengthHigh` tinyint(1) ;
+ALTER TABLE `ArthropodSighting`
+  ADD COLUMN `QCArthropodLengthOK` tinyint(1) ;
+ALTER TABLE `ArthropodSighting`
+  ADD COLUMN `QCArthropodQuantityHigh` tinyint(1) ;
+ALTER TABLE `ArthropodSighting`
+  ADD COLUMN `QCArthropodQuantityOK` tinyint(1) ;
+
+  
