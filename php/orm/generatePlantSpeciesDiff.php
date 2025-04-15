cd183988fba33afd94eae20c1a5ce1e38ea5b179
Luke Opperman <luke@opperman.net>
2025-04-15 10:45:47 -0400

    Plant Species diff generate php

--- php/orm/generatePlantSpeciesDiff.php
+++ php/orm/generatePlantSpeciesDiff.php
@@ -1,52 +1,48 @@
 <?php                                           
 
+require("PlantSpecies.php");
+
 $filename = './plantRules.csv';
 
 $plants = array();
-$conifers = array();
 
-$src = file_get_contents('../../js/plantSpecies.json');
-$canon = json_decode($src, true);
-
+$canon = PlantSpeciesList();
 
 $f = fopen($filename, 'r');
 // Eat first row
 fgetcsv($f);
 
-function canon_find($type, $val) {
+
+function canon_find($common, $sciName) {
   global $canon;
 
-  return count(array_filter($canon[$type], function($match) use ($val) {
-        $match = str_replace(" spp.", "", $match);
-        return strcasecmp($match, $val) == 0;
+  return count(array_filter($canon, function($match) use ($common, $sciName) {
+        return strcasecmp($match[0], $common) == 0 || strcasecmp($match[1], $sciName) == 0;
       }));
 }
 
-
-while (($data = fgetcsv($f)) !== FALSE) {
-  if ($data[8] == '1') {
-    if (!canon_find("conifers", $data[0]) && !canon_find("conifers", $data[1])) {
-      $conifers[] = $data[1];
-    }
-    if (str_word_count($data[3]) > 1 && !canon_find("conifers", $data[3])) {
-      $conifers[] = $data[3];
+function spp($name, $data) {
+  if (strcasecmp($data[6], "genus") == 0) {
+    if (!strpos($name, 'spp')) {
+      $name = $name . " spp.";
     }
   }
-  else {
-    if (!canon_find("plants", $data[0]) && !canon_find("plants", $data[1])) {
-      $plants[] = $data[1];
-    }
-    if (str_word_count($data[3]) > 1 && !canon_find("plants", $data[3])) {
-      $plants[] = $data[3];
-    }
-  }
-
+  return $name;
 }
 
-//$conifers = array_values(array_udiff($conifers, $canon["conifers"], 'strcasecmp'));
-//$plants = array_values(array_udiff($plants, $canon["plants"], 'strcasecmp'));
+while (($data = fgetcsv($f)) !== FALSE) {
+  if ($data[1] == 'NA' || $data[2] == 'NA' || strcasecmp($data[1],'Unknown') == 0 || strcasecmp($data[2],'Unknown') == 0) {
+    continue;
+  }
+  $common = spp($data[1], $data);
+  $sciName = spp($data[2], $data);
+  if (!canon_find($common, $sciName)) {
+    $plants[] = array($common, $sciName, ($data[8] == '1' ? 1 : 0 ));
+  }
+}
 
-print(json_encode(array("plants" => $plants, "conifers" => $conifers), JSON_PRETTY_PRINT));
-
+foreach($plants as $plant) {
+  echo("array(\"$plant[0]\", \"$plant[1]\", $plant[2]),\n");
+}
 
 ?>
